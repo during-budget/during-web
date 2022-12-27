d8ec48db3f58a50ca0bf689449cbe489efbf735f
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2022-12-27 11:18:32 +0900

    feat: Router / create routers (Budget - List, Form, Detail)

--- expense-schedular/src/index.tsx
+++ expense-schedular/src/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    Navigate,
+    RouterProvider,
+} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import BudgetList from './screens/budget/BudgetList';
+import BudgetForm from './screens/budget/BudgetForm';
+import BudgetDetail from './screens/budget/BudgetDetail';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Navigate to="/budget" replace />,
+    },
+    {
+        path: '/budget',
+        element: <BudgetList />,
+    },
+    {
+        path: '/budget/form',
+        element: <BudgetForm />,
+    },
+    {
+        path: '/budget/:budgetId',
+        element: <BudgetDetail />,
+    },
+]);
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
