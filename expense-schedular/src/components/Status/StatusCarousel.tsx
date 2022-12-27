257c776a4dda07d93f09bafbf5c1f947b5822189
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2022-12-27 20:32:51 +0900

    feat: Status / create DateStatus & Calendar component

--- expense-schedular/src/components/Status/StatusCarousel.tsx
+++ expense-schedular/src/components/Status/StatusCarousel.tsx
@@ -1,8 +1,10 @@
 import Carousel from '../UI/Carousel';
+import DateStatus from './DateStatus';
 
 function StatusCarousel(props: { initialIndex: number }) {
     return (
         <Carousel id="status" initialIndex={props.initialIndex} width="20rem">
+            <DateStatus />
         </Carousel>
     );
 }
