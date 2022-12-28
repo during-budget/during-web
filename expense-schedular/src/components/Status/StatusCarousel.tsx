363670f56827de5a5b34d5fb1ae866d36948f390
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2022-12-28 19:58:02 +0900

    feat: Status / create CategoryStatus component #1

--- expense-schedular/src/components/Status/StatusCarousel.tsx
+++ expense-schedular/src/components/Status/StatusCarousel.tsx
@@ -1,12 +1,14 @@
 import Carousel from '../UI/Carousel';
 import DateStatus from './DateStatus';
 import TotalStatus from './TotalStatus';
+import CategoryStatus from './CategoryStatus';
 
 function StatusCarousel(props: { initialIndex: number }) {
     return (
         <Carousel id="status" initialIndex={props.initialIndex} width="20rem">
             <DateStatus />
             <TotalStatus />
+            <CategoryStatus />
         </Carousel>
     );
 }
