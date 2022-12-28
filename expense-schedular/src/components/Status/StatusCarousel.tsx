04aa2af893e25e89342c6837def327a1ea44f0cb
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2022-12-28 15:07:46 +0900

    feat: Status / create TotalStatus & AmountDetail component

--- expense-schedular/src/components/Status/StatusCarousel.tsx
+++ expense-schedular/src/components/Status/StatusCarousel.tsx
@@ -1,10 +1,12 @@
 import Carousel from '../UI/Carousel';
 import DateStatus from './DateStatus';
+import TotalStatus from './TotalStatus';
 
 function StatusCarousel(props: { initialIndex: number }) {
     return (
         <Carousel id="status" initialIndex={props.initialIndex} width="20rem">
             <DateStatus />
+            <TotalStatus />
         </Carousel>
     );
 }
