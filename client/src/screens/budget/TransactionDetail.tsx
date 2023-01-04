d64b15acdc55f8d5fdd1df3c4788902f7325298a
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2023-01-04 17:06:27 +0900

    feat: Style / add responsive style

--- client/src/screens/budget/TransactionDetail.tsx
+++ client/src/screens/budget/TransactionDetail.tsx
@@ -36,109 +36,115 @@ function TransactionDetail() {
 
     return (
         <>
-            <ScrollRestoration />
-            <div className={`page ${classes.detail}`}>
-                <button
-                    className={classes.back}
-                    onClick={() => {
-                        navigation(-1);
-                    }}
-                >
-                    <i className="fa-solid fa-chevron-left"></i>
-                </button>
-                <div className={classes.header}>
-                    <span className={classes.icon}>
-                        {data.icon
-                            ? data.icon
-                            : data.category
-                            ? data.category.icon
-                            : ''}
-                    </span>
-                    <h2>{data.title.join(' | ')}</h2>
+            <div className={classes.container}>
+                <ScrollRestoration />
+                <div className={`page ${classes.detail}`}>
+                    <button
+                        className={classes.back}
+                        onClick={() => {
+                            navigation(-1);
+                        }}
+                    >
+                        <i className="fa-solid fa-chevron-left"></i>
+                    </button>
+                    <div className={classes.header}>
+                        <span className={classes.icon}>
+                            {data.icon
+                                ? data.icon
+                                : data.category
+                                ? data.category.icon
+                                : ''}
+                        </span>
+                        <h2>{data.title.join(' | ')}</h2>
+                    </div>
+                    <p className={classes.amount}>
+                        {data.amount.getCurrentStr()}
+                    </p>
+                    <div className={classes.tabs}>
+                        <ul className="nav-tab">
+                            <li>
+                                <input
+                                    id="transaction-detail-scheduled"
+                                    type="radio"
+                                    name="transaction-detail-isCurrent"
+                                    defaultChecked={true}
+                                ></input>
+                                <label htmlFor="transaction-detail-scheduled">
+                                    예정 내역
+                                </label>
+                            </li>
+                            <li>
+                                <input
+                                    id="transaction-detail-current"
+                                    type="radio"
+                                    name="transaction-detail-isCurrent"
+                                ></input>
+                                <label htmlFor="transaction-detail-current">
+                                    거래 내역
+                                </label>
+                            </li>
+                        </ul>
+                        <span className={classes.divider}>|</span>
+                        <ul className="nav-tab">
+                            <li>
+                                <input
+                                    id="transaction-detail-expense"
+                                    type="radio"
+                                    name="transaction-detail-isExpense"
+                                    defaultChecked={true}
+                                ></input>
+                                <label htmlFor="transaction-detail-expense">
+                                    지출
+                                </label>
+                            </li>
+                            <li>
+                                <input
+                                    id="transaction-detail-income"
+                                    type="radio"
+                                    name="transaction-detail-isExpense"
+                                ></input>
+                                <label htmlFor="transaction-detail-income">
+                                    수입
+                                </label>
+                            </li>
+                        </ul>
+                    </div>
+                    <form className={classes.form}>
+                        <div className="input-field">
+                            <label>분류</label>
+                            <input type="text"></input>
+                        </div>
+                        <div className="input-field">
+                            <label>날짜</label>
+                            <input type="date"></input>
+                        </div>
+                        <div className="input-field">
+                            <label>메모</label>
+                            <textarea></textarea>
+                        </div>
+                        <div className="input-field">
+                            <label>태그</label>
+                            <input></input>
+                        </div>
+                    </form>
                 </div>
-                <p className={classes.amount}>{data.amount.getCurrentStr()}</p>
-                <div className={classes.tabs}>
-                    <ul className="nav-tab">
+                <hr />
+                <div>
+                    <ul className={`nav-tab ${classes.nav}`}>
                         <li>
                             <input
-                                id="transaction-detail-scheduled"
+                                id="transaction-detail-title1"
                                 type="radio"
-                                name="transaction-detail-isCurrent"
+                                name="transaction-detail-list-tab"
                                 defaultChecked={true}
                             ></input>
-                            <label htmlFor="transaction-detail-scheduled">
-                                예정 내역
-                            </label>
-                        </li>
-                        <li>
-                            <input
-                                id="transaction-detail-current"
-                                type="radio"
-                                name="transaction-detail-isCurrent"
-                            ></input>
-                            <label htmlFor="transaction-detail-current">
-                                거래 내역
+                            <label htmlFor="transaction-detail-titl1">
+                                제목1
                             </label>
                         </li>
                     </ul>
-                    <span className={classes.divider}>|</span>
-                    <ul className="nav-tab">
-                        <li>
-                            <input
-                                id="transaction-detail-expense"
-                                type="radio"
-                                name="transaction-detail-isExpense"
-                                defaultChecked={true}
-                            ></input>
-                            <label htmlFor="transaction-detail-expense">
-                                지출
-                            </label>
-                        </li>
-                        <li>
-                            <input
-                                id="transaction-detail-income"
-                                type="radio"
-                                name="transaction-detail-isExpense"
-                            ></input>
-                            <label htmlFor="transaction-detail-income">
-                                수입
-                            </label>
-                        </li>
-                    </ul>
+                    <TransactionList transactions={data.transactions} />
                 </div>
-                <form className={classes.form}>
-                    <div className="input-field">
-                        <label>분류</label>
-                        <input type="text"></input>
-                    </div>
-                    <div className="input-field">
-                        <label>날짜</label>
-                        <input type="date"></input>
-                    </div>
-                    <div className="input-field">
-                        <label>메모</label>
-                        <textarea></textarea>
-                    </div>
-                    <div className="input-field">
-                        <label>태그</label>
-                        <input></input>
-                    </div>
-                </form>
-            </div>
-            <hr />
-            <div>
-                <ul className={`nav-tab ${classes.nav}`}>
-                    <li>
-                        <input
-                            id="transaction-detail-title1"
-                            type="radio"
-                            name="transaction-detail-list-tab"
-                            defaultChecked={true}
-                        ></input>
-                        <label htmlFor="transaction-detail-titl1">제목1</label>
-                    </li>
-                </ul>
-                <TransactionList transactions={data.transactions} />
             </div>
         </>
     );
