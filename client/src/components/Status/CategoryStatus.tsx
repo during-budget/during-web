2fe0904a6d2fe9c17f165d2e58c56edd0881cb04
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2023-01-14 13:02:51 +0900

    feat: Store / update state management with redux

--- client/src/components/Status/CategoryStatus.tsx
+++ client/src/components/Status/CategoryStatus.tsx
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Amount from '../../models/Amount';
 import AmountBars from '../Amount/AmountBars';
 
 function CategoryStatus() {
+    const categoryData = useSelector(
+        (state: any) => state.budget.category
+    );
+
     return (
         <div className="status-container">
             <div className="status-header">
@@ -10,14 +15,9 @@ function CategoryStatus() {
                 <Link to="">{'더보기 >'}</Link>
             </div>
             <AmountBars
-                amountData={[
-                    { amount: new Amount(10000, 60000, 120000), label: '💰' },
-                    { amount: new Amount(100000, 180000, 300000), label: '🚉' },
-                    { amount: new Amount(120000, 180000, 250000), label: '🎉' },
-                    { amount: new Amount(200000, 360000, 400000), label: '🍚' },
-                    { amount: new Amount(30000, 90000, 100000), label: '☕️' },
-                    { amount: new Amount(100000, 160000, 180000), label: '🎓' },
-                ]}
+                amountData={categoryData.map((item: any) => {
+                    return { amount: item.amount, label: item.icon };
+                })}
             />
         </div>
     );
