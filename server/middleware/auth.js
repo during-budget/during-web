908085f225de23663dbd3430dc95eb6820053094
jessie129j@gmail.com <jessie129j@gmail.com>
2023-01-23 00:03:39 +0900

    feat: Create authentication middleware #22

---
+++ server/middleware/auth.js
@@ -0,0 +1,17 @@
+const User = require("../models/User");
+
+exports.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(403).send({ message: "You are not logged in." });
+  }
+};
+
+exports.isNotLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(403).send({ message: "You are already logged in." });
+  }
+};
