564a639f58b864e3c0ec4cd55cde230620111faa
jessie129j@gmail.com <jessie129j@gmail.com>
2023-01-23 00:03:39 +0900

    feat: Create authentication middleware #22

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const users = require("../controllers/users");
+const { isLoggedIn, isNotLoggedIn } = require("../middleware/auth");
 
 // Authentication
-router.post("/register", users.register);
-router.post("/login/local", users.loginLocal);
-router.get("/logout", users.logout);
-router.get("/", users.current);
+router.post("/register", isNotLoggedIn, users.register);
+router.post("/login/local", isNotLoggedIn, users.loginLocal);
+router.get("/logout", isLoggedIn, users.logout);
+router.get("/", isLoggedIn, users.current);
 
 module.exports = router;
