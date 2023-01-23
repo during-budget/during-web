b2f68d9a69b6b263f6fbbb58725ce763f2df73a6
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2023-01-23 14:36:48 +0900

    feat: TransactionItem / create option button & context menu #3

--- client/src/components/Transaction/TransactionItem.tsx
+++ client/src/components/Transaction/TransactionItem.tsx
@@ -1,14 +1,13 @@
 import classes from './TransactionItem.module.css';
 import Tag from '../UI/Tag';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Transaction from '../../models/Transaction';
 import { useSelector } from 'react-redux';
 import Amount from '../../models/Amount';
+import OptionButton from '../UI/OptionButton';
 
 function TransactionItem(props: { transaction: Transaction }) {
     const navigation = useNavigate();
-    const [isShowOverlay, setIsShowOverlay] = useState(false);
 
     const { id, icon, title, amount, categoryId, budgetId, tags } =
         props.transaction;
@@ -20,32 +19,51 @@ function TransactionItem(props: { transaction: Transaction }) {
         throw new Error("Category doesn't exists");
     }
 
-    const clickHandler = () => {
+    const contextMenu = [
+        {
+            name: '거래 내역으로 이동',
+            action: () => {},
+        },
+        {
+            name: '내역 수정',
+            action: () => {},
+        },
+        {
+            name: '내역 삭제',
+            action: () => {},
+        },
+    ];
+
+    const navigateHandler = () => {
         navigation(`/budget/${budgetId}/${id}`);
-        setIsShowOverlay(true);
     };
 
     return (
-        <li className={classes.item} onClick={clickHandler}>
-            <div className={classes.info}>
-                <span className={classes.icon}>{icon || category.icon}</span>
-                <div className={classes.detail}>
-                    <div className={classes.header}>
-                        <p className={classes.category}>{category.title}</p>
-                        <p className={classes.title}>{title.join(' | ')}</p>
-                    </div>
-                    <div className={classes.amount}>
-                        {Amount.getAmountString(amount)}
+        <li className={classes.item}>
+            <div className={classes.data} onClick={navigateHandler}>
+                <div className={classes.info}>
+                    <span className={classes.icon}>
+                        {icon || category.icon}
+                    </span>
+                    <div className={classes.detail}>
+                        <div className={classes.header}>
+                            <p className={classes.category}>{category.title}</p>
+                            <p className={classes.title}>{title.join(' | ')}</p>
+                        </div>
+                        <div className={classes.amount}>
+                            {Amount.getAmountString(amount)}
+                        </div>
                     </div>
                 </div>
+                <div className={classes.tags}>
+                    {tags
+                        ? tags.map((tag: string) => {
+                              return <Tag key={tag}>{tag}</Tag>;
+                          })
+                        : null}
+                </div>
             </div>
-            <div className={classes.tags}>
-                {tags
-                    ? tags.map((tag: string) => {
-                          return <Tag key={tag}>{tag}</Tag>;
-                      })
-                    : null}
-            </div>
+            <OptionButton menu={contextMenu} />
         </li>
     );
 }
