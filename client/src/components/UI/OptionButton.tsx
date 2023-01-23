827d9271d89ac7dfa1fb2bffa80f032ad5beb35c
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2023-01-23 14:36:48 +0900

    feat: TransactionItem / create option button & context menu #3

---
+++ client/src/components/UI/OptionButton.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import classes from './OptionButton.module.css';
+
+function OptionButton(props: { menu: { name: string; action: () => void }[] }) {
+    const [isShowMenu, setIsShowMenu] = useState(false);
+
+    const outside = (
+        <div
+            className={classes.outside}
+            onClick={() => {
+                setIsShowMenu(false);
+            }}
+        ></div>
+    );
+
+    const contextMenu = (
+        <ul className={classes.context}>
+            {props.menu.map((item) => {
+                const { name, action } = item;
+                return (
+                    <li key={name} onClick={action}>
+                        {name}
+                    </li>
+                );
+            })}
+        </ul>
+    );
+
+    return (
+        <>
+            {isShowMenu && outside}
+            <div className={classes.option}>
+                <button
+                    className={classes.button}
+                    onClick={() => {
+                        setIsShowMenu(true);
+                    }}
+                ></button>
+                {isShowMenu && contextMenu}
+            </div>
+        </>
+    );
+}
+
+export default OptionButton;
