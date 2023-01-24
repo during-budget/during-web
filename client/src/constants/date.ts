6dc08be7138902987c5201151ba114e92339b782
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2023-01-25 06:52:47 +0900

    fix: Date / Change start day of week - Sunday

--- client/src/constants/date.ts
+++ client/src/constants/date.ts
@@ -1,5 +1,5 @@
 // TODO: 요일 localization
-const WEEK_DAYS = ['월', '화', '수', '목', '금', '토', '일'];
+const WEEK_DAYS = ['일', '월', '화', '수', '목', '금', '토'];
 const MONTH_DAYS = [...Array(28).keys()].map((i) => i + 1);
 
 export { WEEK_DAYS, MONTH_DAYS };
