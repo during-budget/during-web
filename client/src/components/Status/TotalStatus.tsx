318d9babd1c85b9cdf49059c426db1e3cc469be8
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2023-01-29 11:01:26 +0900

    feat: TotalStatus / Add nav - income, expense #34

--- client/src/components/Status/TotalStatus.tsx
+++ client/src/components/Status/TotalStatus.tsx
@@ -1,4 +1,5 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+import classes from './TotalStatus.module.css';
 import Amount from '../../models/Amount';
 import AmountDetail from '../Amount/AmountDetail';
 import AmountRing from '../Amount/AmountRing';
@@ -7,13 +8,39 @@ function TotalStatus(props: {
     budgetId: string;
     total: { expense: Amount; income: Amount };
 }) {
+    const [isExpense, setIsExpense] = useState(true);
     return (
         <Fragment>
-            <AmountRing amount={props.total.expense} />
+            <ul className={`nav-tab ${classes.nav}`}>
+                <li>
+                    <input
+                        id="total-nav-expense"
+                        type="radio"
+                        name="total-nav"
+                        defaultChecked={true}
+                        onClick={() => {
+                            setIsExpense(true);
+                        }}
+                    ></input>
+                    <label htmlFor="total-nav-expense">지출</label>
+                </li>
+                <li>
+                    <input
+                        id="total-nav-income"
+                        type="radio"
+                        name="total-nav"
+                        onClick={() => {
+                            setIsExpense(false);
+                        }}
+                    ></input>
+                    <label htmlFor="total-nav-income">수입</label>
+                </li>
+            </ul>
+            <AmountRing amount={isExpense ? props.total.expense : props.total.income} />
             <AmountDetail
                 budgetId={props.budgetId}
-                isExpense={true}
-                amount={props.total.expense}
+                isExpense={isExpense}
+                amount={isExpense ? props.total.expense : props.total.income}
             />
         </Fragment>
     );
