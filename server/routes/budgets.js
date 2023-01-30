a08219457eee71de4cd587bb573498a494667507
jessie129j@gmail.com <jessie129j@gmail.com>
2023-01-30 11:37:12 +0900

    feat: Update BudgetAPIs #62

--- server/routes/budgets.js
+++ server/routes/budgets.js
@@ -5,8 +5,13 @@ const { isLoggedIn } = require("../middleware/auth");
 const budgets = require("../controllers/budgets");
 
 router.post("/", isLoggedIn, budgets.create);
-router.put("/:_id/categories/:categoryId", isLoggedIn, budgets.updateCategory);
+
+router.post("/:_id/category", isLoggedIn, budgets.createCategory);
+router.put("/:_id/category/ammount", isLoggedIn, budgets.updateCategoryAmmount);
+router.delete("/:_id/category", isLoggedIn, budgets.removeCategory);
+
 router.patch("/:_id", isLoggedIn, budgets.updateField);
+
 router.get("/:_id?", isLoggedIn, budgets.find);
 router.delete("/:_id", isLoggedIn, budgets.remove);
 
