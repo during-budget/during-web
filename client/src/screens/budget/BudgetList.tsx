68692714ac23e8e96f1651509aea1007c3980e9a
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2023-01-31 07:57:27 +0900

    feat: BudgetList / Add BudgetItems #26

--- client/src/screens/budget/BudgetList.tsx
+++ client/src/screens/budget/BudgetList.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import BudgetItem from '../../components/Budget/BudgetItem';
 import YearPicker from '../../components/Budget/YearPicker';
-import { BUDGET_TYPE } from '../../constants/types';
 import classes from './BudgetList.module.css';
-import { getBudgetData } from '../../util/api';
+import { getBudgetList } from '../../util/api';
 import { useLoaderData } from 'react-router-dom';
 import { budgetActions } from '../../store/budget';
+import Budget from '../../models/Budget';
+
+const startDay = 1;
+const endDay = startDay - 1;
 
 function BudgetList() {
     const dispatch = useDispatch();
@@ -14,13 +17,17 @@ function BudgetList() {
     const [yearState, setYearState] = useState(currentYearStr);
 
     const loaderData: any = useLoaderData();
-    const budgets = useSelector((state: any) => state.budgets);
+    const budgets = useSelector((state: any) => state.budgets).filter(
+        (budget: Budget) => {
+            const startYear = new Date(budget.startDate).getFullYear();
+            const endYear = new Date(budget.endDate).getFullYear();
+            return startYear >= +yearState && endYear <= +yearState;
+        }
+    );
 
     useEffect(() => {
         dispatch(budgetActions.setBudgets(loaderData.budgets));
     }, [loaderData.budgets, dispatch]);
-
-    console.log('hello', budgets);
 
     return (
         <div className={classes.container}>
@@ -30,12 +37,39 @@ function BudgetList() {
                     setYearState(value);
                 }}
             />
+            <ol>
+                {Array(12)
+                    .fill(0)
+                    .map((_, month: number) => {
+                        const startDate = new Date(+yearState, month, startDay);
+                        const endDate = new Date(
+                            +yearState,
+                            endDay ? month : month + 1,
+                            endDay
+                        );
+                        const budget = budgets.find(
+                            (item: Budget) =>
+                                item.startDate.toLocaleDateString() ===
+                                    startDate.toLocaleDateString() &&
+                                item.endDate.toLocaleDateString() ===
+                                    endDate.toLocaleDateString()
+                        );
+                        return (
+                            <BudgetItem
+                                key={month}
+                                startDate={startDate}
+                                endDate={endDate}
+                                budget={budget}
+                            />
+                        );
+                    })}
+            </ol>
         </div>
     );
 }
 
 export const loader = () => {
-    return getBudgetData();
+    return getBudgetList();
 };
 
 export default BudgetList;
