3bbc7274e2515d1a7f8e8dd6e30e56a3c2319c7f
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2023-01-30 13:49:25 +0900

    feat: Auth / Add auto login

--- client/src/screens/auth/Auth.tsx
+++ client/src/screens/auth/Auth.tsx
@@ -1,16 +1,30 @@
-import { useSelector } from 'react-redux';
-import { Navigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate, useLoaderData, useLocation } from 'react-router-dom';
+import { userActions } from '../../store/user';
+import { getCurrentUserState } from '../../util/api';
 import classes from './Auth.module.css';
 import AuthForm from './AuthForm';
 
 function Auth() {
+    const dispatch = useDispatch();
+    const loaderData: any = useLoaderData();
+
     const auth = useSelector((state: any) => state.user.isAuthenticated);
     const location = useLocation();
     const from = location.state?.from?.pathname;
 
+    useEffect(() => {
+        if (loaderData) {
+            dispatch(userActions.login());
+        } else {
+            dispatch(userActions.logout());
+        }
+    }, [loaderData]);
+
     if (auth) {
-        if (from === '/') {
-            return <Navigate to="budget" replace />;
+        if (from === '/' || from === '/auth' || from === undefined) {
+            return <Navigate to="/budget" replace />;
         } else {
             return <Navigate to={from} replace />;
         }
@@ -31,5 +45,9 @@ function Auth() {
         </>
     );
 }
+
+export const loader = () => {
+    return getCurrentUserState();
+};
 
 export default Auth;
