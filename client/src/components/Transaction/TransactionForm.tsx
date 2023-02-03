7aa8a8a1b1cff938e2f024b9c80ec26747d31c5e
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-02-03 12:42:37 +0900

    feat: Transaction / Update transaction #84

--- client/src/components/Transaction/TransactionForm.tsx
+++ client/src/components/Transaction/TransactionForm.tsx
@@ -5,13 +5,12 @@ import OverlayForm from '../UI/form/OverlayForm';
 import TransactionNav from './TransactionNav';
 import Transaction from '../../models/Transaction';
 import { budgetActions } from '../../store/budget';
-import { categoryActions } from '../../store/category';
 import { transactionActions } from '../../store/transaction';
 import { uiActions } from '../../store/ui';
 import CategoryInput from '../UI/input/CategoryInput';
 import TitleInput from '../UI/input/TitleInput';
 import TagInput from '../UI/input/TagInput';
-import { createTransaction } from '../../util/api';
+import { createTransaction, updateTransaction } from '../../util/api';
 import Category from '../../models/Category';
 
 function TransactionForm(props: { budgetId: string }) {
@@ -134,7 +133,12 @@ function TransactionForm(props: { budgetId: string }) {
             memo: memoRef.current!.value,
         });
         dispatch(transactionActions.addTransaction(newTransaction));
-        createTransaction(newTransaction);
+
+        if (formState.isEdit) {
+            updateTransaction(newTransaction);
+        } else {
+            createTransaction(newTransaction);
+        }
     };
 
     const dispatchLink = (linkId: string) => {
