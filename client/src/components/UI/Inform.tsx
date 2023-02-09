02e171f8ea4b51f96b9c14fa13786eb987af66e6
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-02-09 11:47:36 +0900

    feat: Status Components / Add className props

--- client/src/components/UI/Inform.tsx
+++ client/src/components/UI/Inform.tsx
@@ -4,11 +4,12 @@ import classes from './Inform.module.css';
 function Inform(props: {
     isError: boolean;
     children: ReactNode;
+    className?: string;
     hideIcon?: boolean;
 }) {
     const msgClass = props.isError ? classes.error : classes.inform;
     return (
-        <div className={`${classes.msg} ${msgClass}`}>
+        <div className={`${classes.msg} ${msgClass} ${props.className}`}>
             {!props.hideIcon && (
                 <i
                     className={`fa-solid fa-circle-exclamation ${classes.icon}`}
