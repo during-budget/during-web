d6acd12f016484810eb6f35f2b8c3da2e67defb1
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-02-09 16:14:43 +0900

    feat: CategoryStatus / Alert in amount chart & Add onClick for chaning currentIdx #108

--- client/src/components/Amount/AmountBars.tsx
+++ client/src/components/Amount/AmountBars.tsx
@@ -3,7 +3,12 @@ import AmountBar from './AmountBar';
 import Amount from '../../models/Amount';
 
 function AmountBars(props: {
-    amountData: { amount: Amount; label: string }[];
+    amountData: {
+        amount: Amount;
+        label: string;
+        isOver?: boolean;
+        onClick?: (i: number) => void;
+    }[];
     isTop?: boolean;
     isExpense?: boolean;
     className?: string;
@@ -21,9 +26,15 @@ function AmountBars(props: {
                 return (
                     <li
                         key={i}
-                        className={classes.category}
+                        className={classes.bar}
+                        onClick={() => {
+                            data.onClick && data.onClick(i);
+                        }}
                         style={{ width: widths[i] }}
                     >
+                        {data.isOver && (
+                            <i className="fa-solid fa-circle-exclamation"></i>
+                        )}
                         {props.isTop && (
                             <span className={classes.label}>{data.label}</span>
                         )}
