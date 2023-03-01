633350dca978ab4a25593de5eabda366225a0ded
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-01 20:09:19 +0900

    feat: Auth / Set auth route #121

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,27 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+    createBrowserRouter,
+    Navigate,
+    RouterProvider,
+} from 'react-router-dom';
 import Root from './screens/Root';
+import Auth from './screens/Auth';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
-        children: [],
+        children: [
+            {
+                path: '/',
+                index: true,
+                element: <Navigate to="/auth" />,
+            },
+            {
+                path: '/auth',
+                element: <Auth />,
+            },
+        ],
     },
 ]);
 
