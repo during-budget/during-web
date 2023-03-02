d79cf7c996574fdcf8becfe2ffe27d5329fea5fa
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-02 19:05:58 +0900

    feat: Auth / Add user api #128

---
+++ client/src/util/api/userAPI.ts
@@ -0,0 +1,61 @@
+const BASE_URL = 'http://localhost:5555/api/users';
+
+export const registerUser = async (userName: string, password: string) => {
+    const url = `${BASE_URL}/register`;
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            userName,
+            password,
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(`Failed to register.\n${data ? data.message : ''}`);
+    }
+};
+
+export const loginUser = async (userName: string, password: string) => {
+    const url = `${BASE_URL}/login/local`;
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({
+            userName,
+            password,
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(
+            `Failed to login.\n${data.message ? data.message : ''}`
+        );
+    }
+};
+
+export const getUserState = async () => {
+    const url = `${BASE_URL}/current`;
+    const response = await fetch(url, {
+        credentials: 'include',
+    });
+
+    if (!response.ok) {
+        const data = await response.json();
+        console.error(
+            `Yout should login.\n${data.message ? data.message : ''}`
+        );
+        return null;
+    }
+
+    return response.json();
+};
