e41f5fa9e6c276063994852962c25758a646e0b9
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-03 09:46:12 +0900

    feat: Nav / Add nav path #107

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import Root from './screens/Root';
 import Auth from './screens/Auth';
+import Nav from './screens/Nav';
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,18 @@ const router = createBrowserRouter([
             {
                 path: '/auth',
                 element: <Auth />,
+            },
+            {
+                path: '/budget',
+                element: <Nav />,
+            },
+            {
+                path: '/during',
+                element: <Nav />,
+            },
+            {
+                path: '/user',
+                element: <Nav />,
             },
         ],
     },
