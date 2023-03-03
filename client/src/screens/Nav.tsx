b0e058ff621e7060237ae3c74a002dfa7ee6bcdf
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-03 09:45:20 +0900

    feat: Nav / Create nav componenet #107

---
+++ client/src/screens/Nav.tsx
@@ -0,0 +1,32 @@
+import { Link, Outlet, useLocation } from 'react-router-dom';
+import classes from './Nav.module.css';
+
+function Nav() {
+    const loacation = useLocation();
+
+    const navData = [
+        { path: '/budget', icon: <i className="fa-solid fa-home"></i> },
+        { path: '/during', icon: <i className="fa-solid fa-circle-dot"></i> },
+        { path: '/user', icon: <i className="fa-solid fa-user"></i> },
+    ];
+
+    const navList = navData.map((data) => {
+        const isCurrentPath = loacation.pathname.includes(data.path);
+        const className = `${classes.link} ${isCurrentPath && classes.active}`;
+
+        return (
+            <Link key={data.path} to={data.path} className={className}>
+                {data.icon}
+            </Link>
+        );
+    });
+
+    return (
+        <div>
+            <Outlet />
+            <nav className={classes.nav}>{navList}</nav>
+        </div>
+    );
+}
+
+export default Nav;
