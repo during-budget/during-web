a8ad9d54ad5edaff7fa87a8b1e8307f8c16842a1
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-03 16:00:34 +0900

    feat: Budget / Create budget slice & Add setBudget reducer #136

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user';
+import budgetReducer from './budget';
 
 const store = configureStore({
     reducer: {
         user: userReducer,
+        budgets: budgetReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 });
 
 export default store;
