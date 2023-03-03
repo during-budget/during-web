119414e3beaa350e044fc11fa0b8371ba2a9e49a
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-03 15:57:12 +0900

    feat: Model / Create models - Budget, Amount, Category #136

---
+++ client/src/models/Amount.ts
@@ -0,0 +1,70 @@
+class Amount {
+    private _current: number;
+    private _scheduled: number;
+    private _planned: number;
+    private _state: {
+        target: string;
+        over: string;
+        isOver: boolean;
+        amount: number;
+    }[];
+
+    get current() {
+        return this._current;
+    }
+
+    get scheduled() {
+        return this._scheduled;
+    }
+
+    get planned() {
+        return this._planned;
+    }
+
+    get state() {
+        return this._state;
+    }
+
+    private getEachState = (
+        target: string,
+        over: string,
+        overAmount: number
+    ) => {
+        const isOver = overAmount < 0;
+        return {
+            target,
+            over,
+            isOver,
+            amount: isOver ? -overAmount : 0,
+        };
+    };
+
+    private getState = () => {
+        const currentOverSchedule = this.getEachState(
+            '현재 내역',
+            '예정',
+            this._scheduled - this._current
+        );
+        const currentOverPlanned = this.getEachState(
+            '현재 내역',
+            '목표',
+            this._planned - this._current
+        );
+        const scheduledOverplanned = this.getEachState(
+            '예정 내역',
+            '목표',
+            this._planned - this.scheduled
+        );
+
+        return [currentOverSchedule, currentOverPlanned, scheduledOverplanned];
+    };
+
+    constructor(current: number, scheduled: number, planned: number) {
+        this._current = current;
+        this._scheduled = scheduled;
+        this._planned = planned;
+        this._state = this.getState();
+    }
+}
+
+export default Amount;
---
+++ client/src/models/Category.ts
@@ -0,0 +1,53 @@
+import Amount from './Amount';
+
+class Category {
+    private _id: string;
+    private _title: string;
+    private _icon: string;
+    private _isExpense: boolean;
+    private _isIncome: boolean;
+    private _amount: Amount | undefined;
+
+    get id() {
+        return this._id;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get icon() {
+        return this._icon;
+    }
+
+    get isExpense() {
+        return this._isExpense;
+    }
+
+    get isIncome() {
+        return this._isIncome;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    constructor(category: {
+        id: string;
+        title: string;
+        icon: string;
+        isExpense: boolean;
+        isIncome: boolean;
+        amount?: Amount;
+    }) {
+        const { id, title, icon, isExpense, isIncome, amount } = category;
+        this._id = id;
+        this._title = title;
+        this._icon = icon;
+        this._isExpense = isExpense;
+        this._isIncome = isIncome;
+        this._amount = amount ? amount : undefined;
+    }
+}
+
+export default Category;
