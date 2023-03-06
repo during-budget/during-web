52b0d92dc517d62c5410c7aa7da7fae51aaef0d1
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-06 17:13:18 +0900

    feat: UI / Create ExpenseTab component #149

---
+++ client/src/components/UI/ExpenseTab.tsx
@@ -0,0 +1,30 @@
+import RadioTab from './RadioTab';
+
+function ExpenseTab(props: {
+    id: string;
+    isExpense: boolean;
+    setIsExpense: (isExpense: boolean) => void;
+}) {
+    const tabs = [
+        {
+            label: '지출',
+            value: 'expense',
+            defaultChecked: props.isExpense,
+            onChange: () => {
+                props.setIsExpense(true);
+            },
+        },
+        {
+            label: '수입',
+            value: 'income',
+            defaultChecked: !props.isExpense,
+            onChange: () => {
+                props.setIsExpense(false);
+            },
+        },
+    ];
+
+    return <RadioTab name={props.id} values={tabs} />;
+}
+
+export default ExpenseTab;
