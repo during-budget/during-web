4055ad563f3a03ea6d32d4b5a334a5e63fb63ea2
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-06 20:26:03 +0900

    feat: Transaction / Create TransactionNav component #153

---
+++ client/src/components/Transaction/TransactionNav.tsx
@@ -0,0 +1,31 @@
+import RadioTab from '../UI/RadioTab';
+
+function TransactionNav(props: {
+    id: string;
+    isCurrent: boolean;
+    isLine?: boolean;
+    setIsCurrent: (isCurrent: boolean) => void;
+}) {
+    const tabs = [
+        {
+            label: '예정내역',
+            value: 'scheduled',
+            defaultChecked: !props.isCurrent,
+            onChange: () => {
+                props.setIsCurrent(false);
+            },
+        },
+        {
+            label: '거래내역',
+            value: 'current',
+            defaultChecked: props.isCurrent,
+            onChange: () => {
+                props.setIsCurrent(true);
+            },
+        },
+    ];
+
+    return <RadioTab name={props.id} values={tabs} isLine={props.isLine} />;
+}
+
+export default TransactionNav;
