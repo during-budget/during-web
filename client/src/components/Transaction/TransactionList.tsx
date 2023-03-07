b4d7e01c25c871b08742764ec448ab11fa950f6d
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-07 17:05:43 +0900

    feat: Transaction / Create components - layout, list, item #153

---
+++ client/src/components/Transaction/TransactionList.tsx
@@ -0,0 +1,45 @@
+import Transaction from '../../models/Transaction';
+import classes from './TransactionList.module.css';
+import { getNumericHypenDateString } from '../../util/date';
+import TransactionItem from './TransactionItem';
+
+function TransactionList(props: {
+    transactions: Transaction[];
+    isCurrent: boolean;
+}) {
+    const { transactions, isCurrent } = props;
+    const transactionList = Transaction.getTransacitonsFilteredByDate({
+        transactions,
+        isCurrent,
+    });
+
+    return (
+        <ol className={classes.container}>
+            {transactionList.map((data) => {
+                const { date, transactions } = data;
+                return (
+                    <li key={date}>
+                        <ol>
+                            {/* Date */}
+                            <h5
+                                id={getNumericHypenDateString(new Date(date))}
+                                className={classes.date}
+                            >
+                                {date}
+                            </h5>
+                            {/* Transactions */}
+                            {transactions.map((item: Transaction) => (
+                                <TransactionItem
+                                    key={item.id}
+                                    transaction={item}
+                                />
+                            ))}
+                        </ol>
+                    </li>
+                );
+            })}
+        </ol>
+    );
+}
+
+export default TransactionList;
