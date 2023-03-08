cc1fd51996e8e97fda4a3f1eb744cdc0f260bff9
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-08 10:01:45 +0900

    feat: Status / Create StatusHeader component #160

--- client/src/components/Status/CategoryStatus.tsx
+++ client/src/components/Status/CategoryStatus.tsx
@@ -1,5 +1,40 @@
+import { useState } from 'react';
+import StatusHeader from './StatusHeader';
+
 function CategoryStatus() {
-    return <>Category</>;
+    const [isExpense, setIsExpense] = useState(true);
+    const [currentCategoryIdx, setCurrentCategoryIdx] = useState(0);
+
+    const tabs = [
+        {
+            label: '지출',
+            value: 'expense',
+            onChange: () => {
+                setIsExpense(true);
+                setCurrentCategoryIdx(0);
+            },
+            defaultChecked: isExpense,
+        },
+        {
+            label: '수입',
+            value: 'income',
+            onChange: () => {
+                setIsExpense(false);
+                setCurrentCategoryIdx(0);
+            },
+            defaultChecked: !isExpense,
+        },
+    ];
+
+    return (
+        <>
+            <StatusHeader
+                id="category-status-type"
+                title="카테고리별 예산"
+                values={tabs}
+            />
+        </>
+    );
 }
 
 export default CategoryStatus;
