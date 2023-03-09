5ccc1b863aa011e4d33d9105c5d040a969677652
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-09 22:01:01 +0900

    feat: DateStatus / Add header - monthly, weekly #162

--- client/src/components/Status/DateStatus.tsx
+++ client/src/components/Status/DateStatus.tsx
@@ -1,11 +1,15 @@
 import classes from './DateStatus.module.css';
 import Transaction from '../../models/Transaction';
 import Calendar from '../UI/Calendar';
+import StatusHeader from './StatusHeader';
+import { useState } from 'react';
 
 function DateStatus(props: {
     date: { start: Date; end: Date };
     transactions: Transaction[];
 }) {
+    const [isMonthly, setIsMonthly] = useState(true);
+
     const getDateStatus = (date: Date) => {
         let expenseTotal = 0;
         let incomeTotal = 0;
@@ -23,7 +27,7 @@ function DateStatus(props: {
         });
 
         return (
-            <div className={classes.status}>
+            <div className={classes.content}>
                 {expenseTotal ? (
                     <p className={classes.expense}>
                         {'-' + expenseTotal.toLocaleString()}
@@ -42,8 +46,33 @@ function DateStatus(props: {
         );
     };
 
+    const headerTabs = [
+        {
+            label: '월간',
+            value: 'monthly',
+            onChange: () => {
+                setIsMonthly(true);
+            },
+            defaultChecked: isMonthly,
+        },
+        {
+            label: '주간',
+            value: 'weekly',
+            onChange: () => {
+                setIsMonthly(false);
+            },
+            defaultChecked: !isMonthly,
+        },
+    ];
+
     return (
         <>
+            <StatusHeader
+                className={classes.header}
+                id="date-status-type"
+                title="날짜별 현황"
+                values={headerTabs}
+            />
             <Calendar
                 startDate={props.date.start}
                 endDate={props.date.end}
