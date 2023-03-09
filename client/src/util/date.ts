10de75861e8ffc7440b4d57673acfa67f1e2581e
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-09 12:21:20 +0900

    feat: UI / Create Calendar component #162

--- client/src/util/date.ts
+++ client/src/util/date.ts
@@ -1,3 +1,8 @@
+const WEEK_DAYS = {
+    'ko-KR': ['일', '월', '화', '수', '목', '금', '토'],
+    'en-US': ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
+};
+
 // yyyy. mm. dd.
 export const getNumericDotDateString = (date: Date) => {
     return getLocaleNumericString(date, 'ko-KR');
@@ -14,4 +19,19 @@ export const getLocaleNumericString = (date: Date, locale: string) => {
         month: 'numeric',
         day: 'numeric',
     });
+};
+
+export const getLongMonth = (date: Date, locale: string) => {
+    return date.toLocaleString(locale, { month: 'long' });
+};
+
+export const getWeekDays = (locale: string) => {
+    switch (locale) {
+        case 'ko-KR':
+            return WEEK_DAYS['ko-KR'];
+        case 'en-US':
+            return WEEK_DAYS['en-US'];
+        default:
+            return WEEK_DAYS['en-US'];
+    }
 };
