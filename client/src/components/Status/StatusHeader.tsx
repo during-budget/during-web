48c4179f365cb717192adbffd11bde02441b388a
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-09 22:01:01 +0900

    feat: DateStatus / Add header - monthly, weekly #162

--- client/src/components/Status/StatusHeader.tsx
+++ client/src/components/Status/StatusHeader.tsx
@@ -1,9 +1,9 @@
 import RadioTab from '../UI/RadioTab';
 import classes from './StatusHeader.module.css';
 
-function StatusHeader(props: { id: string; title: string; values: any }) {
+function StatusHeader(props: { id: string; title: string; values: any; className?: string }) {
     return (
-        <div className={classes.container}>
+        <div className={`${classes.container} ${props.className}`}>
             <h5>{props.title}</h5>
             <RadioTab name={props.id} values={props.values} />
         </div>
