12b05752310000a5a440c5418799892af5549be6
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-12 09:38:59 +0900

    feat: Overlay / Add isOpen, isClip props #174

--- client/src/components/UI/Overlay.tsx
+++ client/src/components/UI/Overlay.tsx
@@ -1,23 +1,29 @@
 import classes from './Overlay.module.css';
 
 function Overlay(props: {
+    isOpen: boolean;
+    isClip?: boolean;
     onClose?: () => void;
     isShowBackdrop?: boolean;
     children?: React.ReactNode;
     className?: string;
 }) {
     return (
-        <>
+        <div
+            className={`${classes.container} ${
+                props.isOpen ? classes.open : ''
+            } ${props.isClip ? classes.clip : ''}`}
+        >
             <div
                 className={`${classes.backdrop} ${
-                    props.isShowBackdrop && classes.show
+                    props.isShowBackdrop ? classes.show : ''
                 }`}
                 onClick={props.onClose}
             ></div>
             <div className={`${classes.overlay} ${props.className}`}>
                 {props.children}
             </div>
-        </>
+        </div>
     );
 }
 
