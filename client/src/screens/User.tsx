8fdf0d018302eaa168a3701826747bd7e54dbea0
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-12 14:59:58 +0900

    feat: User / Create user screen #47

---
+++ client/src/screens/User.tsx
@@ -0,0 +1,43 @@
+import classes from './User.module.css';
+import UserHeader from '../components/User/Profile/UserHeader';
+import SettingList from '../components/User/Setting/SettingList';
+
+const SETTING_DATA = [
+    {
+        title: '가계부 설정',
+        items: [
+            { icon: '💰', label: '기본 예산 설정', to: '' },
+            { icon: '📅', label: '월 시작일 설정', to: '' },
+            { icon: '🔖', label: '카테고리 설정', to: '' },
+        ],
+    },
+    {
+        title: '기본 설정',
+        items: [
+            { icon: '➕', label: '크기 설정', to: '' },
+            { icon: '🎨', label: '색상 설정', to: '' },
+            { icon: '🌏', label: '언어 설정', to: '' },
+        ],
+    },
+    {
+        title: '회원 설정',
+        items: [{ icon: '👤', label: '회원 정보 수정하기', to: '' }],
+    },
+];
+
+function User() {
+    return (
+        <>
+            <UserHeader userName="이름" email="username@gmail.com" />
+            <main className={classes.container}>
+                <section>
+                    {SETTING_DATA.map((data) => (
+                        <SettingList title={data.title} items={data.items} />
+                    ))}
+                </section>
+            </main>
+        </>
+    );
+}
+
+export default User;
