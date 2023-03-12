45be3bacd059a4c3b507c07bc16117c63781c22f
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-12 09:39:57 +0900

    feat: EmojiInput / Update emojiInput - open animation #174

--- client/src/components/Budget/Input/EmojiInput.tsx
+++ client/src/components/Budget/Input/EmojiInput.tsx
@@ -4,29 +4,31 @@ import data from 'emoji_data_kr';
 import Picker from '@emoji-mart/react';
 import { BiEraser } from 'react-icons/bi';
 import { MdOutlineCancel } from 'react-icons/md';
+import Overlay from '../../UI/Overlay';
 
-const EmojiInput = (props: {id: string, className?: string}) => {
+const EmojiInput = (props: { id: string; className?: string }) => {
     const [open, setOpen] = useState(false);
 
     const iconRef = useRef<HTMLInputElement>(null);
 
-    // Note: 이모티콘 팝업창 닫기 함수
+    // NOTE: 이모티콘 팝업창 닫기 함수
     const cancelHandler = () => {
         setOpen(false);
     };
 
-    // Note: 선택한 이모티콘 값 삭제 함수
+    // NOTE: 선택한 이모티콘 값 삭제 함수
     const deleteIconHandler = () => {
-        iconRef.current!.value = "";
+        iconRef.current!.value = '';
+        setOpen(false);
     };
 
-    // Note: 이모티콘 클릭 시 동작하는 함수
+    // NOTE: 이모티콘 클릭 시 동작하는 함수
     const onEmojiClick = (value: any) => {
         iconRef.current!.value = value.native;
         cancelHandler();
     };
 
-    // Note : 이모지 팝업 오픈 함수
+    // NOTE : 이모지 팝업 오픈 함수
     const handleEmojiPopup = () => {
         setOpen(!open);
     };
@@ -36,34 +38,37 @@ const EmojiInput = (props: {id: string, className?: string}) => {
             <input
                 ref={iconRef}
                 className={classes.icon}
-                type='text'
-                placeholder='💰'
+                type="text"
+                placeholder="💰"
                 maxLength={2}
                 onClick={handleEmojiPopup}
             />
-            {open ? (
-                <div className={classes.overlay}>
-                    <div className={classes.header}>
-                        <BiEraser
-                            className={classes.icons}
-                            onClick={deleteIconHandler}
-                        />
-                        <MdOutlineCancel
-                            className={classes.icons}
-                            onClick={cancelHandler}
-                        />
-                    </div>
-                    <Picker
-                        data={data}
-                        locale='kr'
-                        onEmojiSelect={onEmojiClick}
-                        navPosition='bottom'
-                        previewPosition='none'
-                        skinTonePosition='none'
-                        dynamicWidth='true'
+            <Overlay
+                className={classes.overlay}
+                isOpen={open}
+                isShowBackdrop={true}
+                onClose={cancelHandler}
+            >
+                <div className={classes.header}>
+                    <BiEraser
+                        className={classes.icon}
+                        onClick={deleteIconHandler}
+                    />
+                    <MdOutlineCancel
+                        className={classes.icon}
+                        onClick={cancelHandler}
                     />
                 </div>
-            ) : null}
+                <Picker
+                    data={data}
+                    locale="kr"
+                    onEmojiSelect={onEmojiClick}
+                    navPosition="bottom"
+                    previewPosition="none"
+                    skinTonePosition="search"
+                    dynamicWidth={true}
+                />
+            </Overlay>
         </div>
     );
 };
