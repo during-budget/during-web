77aafe24bc22392f90949719cd5f474ccce0a383
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-18 19:04:58 +0900

    feat: Create transactionSlice #180

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user';
 import budgetReducer from './budget';
 import categoryReducer from './category';
+import transactionReducer from './transaction';
 
 const store = configureStore({
     reducer: {
         user: userReducer,
         budget: budgetReducer,
         category: categoryReducer,
+        transaction: transactionReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
