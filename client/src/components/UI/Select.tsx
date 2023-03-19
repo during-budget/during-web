9e4e06200e62202e19fffbd96e0973aa10fe720d
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-19 12:43:38 +0900

    feat: Select / Add expand state #111

--- client/src/components/UI/Select.tsx
+++ client/src/components/UI/Select.tsx
@@ -1,4 +1,4 @@
-import React, { useImperativeHandle, useRef } from 'react';
+import React, { useImperativeHandle, useRef, useState } from 'react';
 import classes from './Select.module.css';
 
 const Select = React.forwardRef(
@@ -20,28 +20,45 @@ const Select = React.forwardRef(
         });
 
         const selectRef = useRef<HTMLSelectElement>(null);
+        const [isExpand, setIsExpand] = useState(false);
+
+        const toggleList = () => {
+            setIsExpand((prev) => !prev);
+        };
+
+        const closeList = () => {
+            setIsExpand(false);
+        };
+
+        const expandClass = isExpand ? classes.expand : '';
 
         return (
-            <div className={`${classes.container} ${props.className}`}>
-                <select
-                    ref={selectRef}
-                    defaultValue={props.defaultValue}
-                    onChange={props.onChange}
-                    disabled
-                >
-                    {props.data.map((item, i) => (
-                        <option key={i} value={item.value}>
-                            {item.label}
-                        </option>
-                    ))}
-                </select>
-                <ul>
-                    <div className={classes.list}>
-                        {props.data.map((item, i) => {
-                            return <li key={i}>{item.label}</li>;
-                        })}
-                    </div>
-                </ul>
+            <div
+                className={`${classes.container} ${expandClass} ${props.className}`}
+            >
+                <div className={classes.backdrop} onClick={closeList} />
+                <div className={classes.wrapper} onClick={toggleList}>
+                    <div className={classes.clickable} />
+                    <select
+                        ref={selectRef}
+                        defaultValue={props.defaultValue}
+                        onChange={props.onChange}
+                        disabled
+                    >
+                        {props.data.map((item, i) => (
+                            <option key={i} value={item.value}>
+                                {item.label}
+                            </option>
+                        ))}
+                    </select>
+                    <ul>
+                        <div className={classes.list}>
+                            {props.data.map((item, i) => {
+                                return <li key={i}>{item.label}</li>;
+                            })}
+                        </div>
+                    </ul>
+                </div>
             </div>
         );
     }
