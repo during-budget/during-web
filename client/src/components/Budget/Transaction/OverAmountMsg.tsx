835a4411c89f4128eaded59ec82604ec881fec7f
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-21 12:28:22 +0900

    feat: Transaction / Create OverAmount component #176

---
+++ client/src/components/Budget/Transaction/OverAmountMsg.tsx
@@ -0,0 +1,22 @@
+import classes from './OverAmountMsg.module.css';
+
+function OverAmountMsg(props: { overAmount: number; className?: string }) {
+    const overAmount = props.overAmount;
+
+    let overAmountMsg = '';
+    if (overAmount < 0) {
+        overAmountMsg = `예정보다 ${-1 * overAmount}원 절약`;
+    } else if (overAmount > 0) {
+        overAmountMsg = `예정보다 ${overAmount}원 초과`;
+    } else {
+        overAmountMsg = `예정대로 실행`;
+    }
+
+    return (
+        <p className={`${classes.container} ${props.className}`}>
+            {overAmountMsg}
+        </p>
+    );
+}
+
+export default OverAmountMsg;
