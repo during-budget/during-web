964a2ab7d7ae6b5a74183b71b700712a45fc2bd6
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-23 12:01:38 +0900

    feat: TagInput / Implement tag add & remove #117

--- client/src/components/Budget/Input/TagInput.tsx
+++ client/src/components/Budget/Input/TagInput.tsx
@@ -1,4 +1,6 @@
-import React, { useImperativeHandle, useRef } from 'react';
+import React, { useImperativeHandle, useRef, useState } from 'react';
+import classes from './TagInput.module.css';
+import Tag from '../../UI/Tag';
 
 const TagInput = React.forwardRef(
     (
@@ -10,20 +12,86 @@ const TagInput = React.forwardRef(
     ) => {
         useImperativeHandle(ref, () => {
             return {
-                value: () => [tagsRef.current!.value].filter((item) => item),
+                value: () => tagState,
             };
         });
 
-        const tagsRef = useRef<HTMLInputElement>(null);
+        const [tagState, setTagState] = useState<String[]>(
+            props.defaultValue || []
+        );
+
+        const keyUpHandler = (event: React.KeyboardEvent) => {
+            const input = event.target as HTMLInputElement;
+
+            const addKey = event.key === ' ' || event.key === 'Enter';
+            const removeKey = event.key === 'Backspace' && input.value === '';
+
+            if (addKey) {
+                setTagState((prevState: any) => {
+                    const value = input.value;
+                    input.value = '';
+
+                    // check empty input
+                    if (!value || value === ' ') {
+                        return prevState;
+                    }
+
+                    // check same item
+                    const sameItem = prevState.find(
+                        (item: string) => item === value
+                    );
+
+                    // set state
+                    if (sameItem) {
+                        return prevState;
+                    } else {
+                        return [...prevState, value];
+                    }
+                });
+            } else if (removeKey) {
+                setTagState((prevState) => {
+                    // remove last item
+                    return prevState.slice(0, -1) || [];
+                });
+            }
+        };
+
+        const removeHandler = (event: React.MouseEvent) => {
+            const target = event.target as HTMLSpanElement;
+
+            setTagState((prevState) => {
+                return prevState.filter((item) => {
+                    return item.trim() !== target.innerText.trim();
+                });
+            });
+        };
+
+        const preventSubmit = (event: React.KeyboardEvent) => {
+            if (event.key === 'Enter') {
+                event.preventDefault();
+            }
+        };
+
+        const emptyClass = tagState.length === 0 ? classes.empty : '';
+        const containerClass = [classes.container, emptyClass].join(' ');
 
         return (
-            <input
-                ref={tagsRef}
-                className={props.className}
-                type="text"
-                placeholder="태그를 입력하세요"
-                defaultValue={props.defaultValue![0]}
-            />
+            <ul className={containerClass}>
+                {tagState.map((item, i) => (
+                    <li key={i} onClick={removeHandler}>
+                        <Tag isDark={true}>{item}</Tag>
+                    </li>
+                ))}
+                <li>
+                    <input
+                        className={props.className}
+                        type="text"
+                        onKeyUp={keyUpHandler}
+                        onKeyDown={preventSubmit}
+                        placeholder="태그를 입력하세요"
+                    />
+                </li>
+            </ul>
         );
     }
 );
