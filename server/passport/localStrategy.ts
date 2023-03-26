9d810e9898e651c0a125d65f2ae871a281ba338a
jessie129j@gmail.com <jessie129j@gmail.com>
2023-03-26 15:10:12 +0900

    feat: Update local strategy to use code instead of password #201

--- server/passport/localStrategy.ts
+++ server/passport/localStrategy.ts
@@ -1,29 +1,40 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import { User } from "../models/User";
+import { client } from "../redis";
+import { decipher } from "../utils/crypto";
 
 const local = () => {
   passport.use(
     new LocalStrategy(
       {
         usernameField: "email",
-        passwordField: "password",
+        passwordField: "code",
       },
-      async function (email: string, password: string, done: any) {
+      async function (email: string, code: string, done: any) {
         const user = await User.findOne({
           email,
-        }).select("+password");
+        });
         if (!user) {
           const err = new Error("User not found");
           err.status = 404;
           return done(err, null, null);
         }
-        const isMatch = await user.comparePassword(password);
-        if (!isMatch) {
-          const err = new Error("Incorrect password");
+
+        const _code = await client.v4.hGet(email, "code");
+        if (!_code) {
+          const err = new Error("Verification code is expired");
+          err.status = 404;
+          return done(err, null, null);
+        }
+
+        if (decipher(_code) !== code) {
+          const err = new Error("Verification code is wrong");
           err.status = 409;
           return done(err, null, null);
         }
+        await client.del(email);
+
         return done(null, user);
       }
     )
