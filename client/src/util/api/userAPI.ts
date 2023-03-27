2c143b87eef06f4b7f482e972655fb4cd7393355
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-27 17:00:27 +0900

    feat: Auth / Update userAPI for verify using code #206

--- client/src/util/api/userAPI.ts
+++ client/src/util/api/userAPI.ts
@@ -1,13 +1,11 @@
 const BASE_URL = 'http://localhost:5555/api/users';
 
-export const registerUser = async (userName: string, password: string) => {
+export const sendCodeRegister = async (email: string) => {
     const url = `${BASE_URL}/register`;
     const response = await fetch(url, {
         method: 'POST',
-        body: JSON.stringify({
-            userName,
-            password,
-        }),
+        credentials: 'include',
+        body: JSON.stringify({ email }),
         headers: {
             'Content-Type': 'application/json',
         },
@@ -16,19 +14,76 @@ export const registerUser = async (userName: string, password: string) => {
     const data = await response.json();
 
     if (!response.ok) {
-        throw new Error(`Failed to register.\n${data ? data.message : ''}`);
+        throw new Error(
+            `Failed to send code for register.\n${
+                data.message ? data.message : ''
+            }`
+        );
     }
+
+    return data;
 };
 
-export const loginUser = async (email: string, password: string) => {
+export const verifyRgister = async (
+    email: string,
+    code: string,
+    persist: boolean
+) => {
+    const url = `${BASE_URL}/register/verify`;
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({ email, code, persist }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(
+            `Failed to register.\n${data.message ? data.message : ''}`
+        );
+    }
+
+    return data;
+};
+
+export const sendCodeLogin = async (email: string) => {
     const url = `${BASE_URL}/login/local`;
     const response = await fetch(url, {
         method: 'POST',
         credentials: 'include',
-        body: JSON.stringify({
-            email,
-            password,
-        }),
+        body: JSON.stringify({ email }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(
+            `Failed to send code for login.\n${
+                data.message ? data.message : ''
+            }`
+        );
+    }
+
+    return data;
+};
+
+export const verifyLogin = async (
+    email: string,
+    code: string,
+    persist: boolean
+) => {
+    const url = `${BASE_URL}/login/local/verify`;
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({ email, code, persist }),
         headers: {
             'Content-Type': 'application/json',
         },
