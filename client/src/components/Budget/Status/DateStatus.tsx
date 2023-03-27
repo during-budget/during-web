8ad8241d930366a5369c93b9ced8f59ace6dc5f8
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-27 22:41:28 +0900

    feat: DateStatus / Update with weekly calendar #195

--- client/src/components/Budget/Status/DateStatus.tsx
+++ client/src/components/Budget/Status/DateStatus.tsx
@@ -1,98 +1,24 @@
+import { useState } from 'react';
 import classes from './DateStatus.module.css';
 import Transaction from '../../../models/Transaction';
+import { getWeekNames, getWeekNumbers } from '../../../util/date';
 import Calendar from '../../UI/Calendar';
-import StatusHeader from './StatusHeader';
-import { useState } from 'react';
-import RadioTab from '../../UI/RadioTab';
-import { useDispatch } from 'react-redux';
-import { uiActions } from '../../../store/ui';
-import { transactionActions } from '../../../store/transaction';
 import IndexNav from '../../UI/IndexNav';
+import RadioTab from '../../UI/RadioTab';
+import StatusHeader from './StatusHeader';
 
 function DateStatus(props: {
+    title: string;
     date: { start: Date; end: Date };
     transactions: Transaction[];
 }) {
-    const dispatch = useDispatch();
+    const { title, date, transactions } = props;
 
     const [isMonthly, setIsMonthly] = useState(true);
     const [weekNum, setWeekNum] = useState(0);
     const [isView, setIsView] = useState(true);
 
-    const getDateStatus = (date: Date) => {
-        let expenseTotal = 0;
-        let incomeTotal = 0;
-        props.transactions.forEach((item) => {
-            if (
-                item.isCurrent &&
-                item.date.toLocaleDateString() === date.toLocaleDateString()
-            ) {
-                if (item.isExpense) {
-                    expenseTotal += item.amount;
-                } else {
-                    incomeTotal += item.amount;
-                }
-            }
-        });
-
-        return (
-            <div className={classes.content}>
-                {expenseTotal ? (
-                    <p className={classes.expense}>
-                        {'-' + expenseTotal.toLocaleString()}
-                    </p>
-                ) : (
-                    ''
-                )}
-                {incomeTotal ? (
-                    <p className={classes.income}>
-                        {'+' + incomeTotal.toLocaleString()}
-                    </p>
-                ) : (
-                    ''
-                )}
-            </div>
-        );
-    };
-
-    const clickHandler = async (event: React.MouseEvent) => {
-        const date = getDate(event);
-
-        if (isView) {
-            await dispatch(uiActions.setIsCurrent(true));
-            const dateList = document.getElementById(date);
-            dateList?.scrollIntoView({ behavior: 'smooth' });
-        } else {
-            dispatch(
-                transactionActions.setForm({
-                    mode: { isExpand: true },
-                    default: { date },
-                })
-            );
-        }
-    };
-
-    const getDate = (event: React.MouseEvent) => {
-        let date: string | null;
-
-        const eventTarget = event.target as HTMLElement;
-        if (eventTarget.nodeName === 'P') {
-            date = (
-                eventTarget.parentNode?.parentNode
-                    ?.parentNode as HTMLTableDataCellElement
-            ).getAttribute('data-date');
-        } else if (eventTarget.nodeName === 'SPAN') {
-            date = (
-                eventTarget.parentNode?.parentNode as HTMLTableDataCellElement
-            ).getAttribute('data-date');
-        } else if (eventTarget.nodeName === 'TD') {
-            date = (eventTarget as HTMLTableDataCellElement).getAttribute(
-                'data-date'
-            );
-        }
-        return date!;
-    };
-
+    // Tabs
     const headerTabs = [
         {
             label: '월간',
@@ -128,6 +54,45 @@ function DateStatus(props: {
         },
     ];
 
+    // Monthly
+    const monthlyStatus = (
+        <>
+            <Calendar
+                startDate={date.start}
+                endDate={date.end}
+                locale={navigator.language}
+            />
+            <RadioTab name="date-status-action" values={actionTabs} />
+        </>
+    );
+
+    // Weekly
+    const weekNames = getWeekNames(
+        title,
+        date.start,
+        date.end,
+        navigator.language
+    );
+    const weeklyStatus = (
+        <>
+            <Calendar
+                startDate={date.start}
+                endDate={date.end}
+                isMonthTop={true}
+                weekIdx={weekNum}
+                locale={navigator.language}
+            />
+            <IndexNav idx={weekNum} setIdx={setWeekNum} data={weekNames} />
+            <ul>
+                {getWeekNumbers(date.start, date.end, navigator.language).map(
+                    (item: string, i: number) => (
+                        <li key={i}>{item}</li>
+                    )
+                )}
+            </ul>
+        </>
+    );
+
     return (
         <>
             <StatusHeader
@@ -136,29 +101,7 @@ function DateStatus(props: {
                 title="날짜별 현황"
                 values={headerTabs}
             />
-            <Calendar
-                startDate={props.date.start}
-                endDate={props.date.end}
-                getDateStatus={getDateStatus}
-                onClick={clickHandler}
-                week={!isMonthly && weekNum}
-            />
-            {!isMonthly && (
-                <IndexNav
-                    idx={weekNum}
-                    setIdx={setWeekNum}
-                    data={[
-                        '3월 첫째주',
-                        '3월 둘째주',
-                        '3월 셋째주',
-                        '3월 넷째주',
-                        '3월 다섯째주',
-                    ]}
-                />
-            )}
-            {isMonthly && (
-                <RadioTab name="date-status-action" values={actionTabs} />
-            )}
+            {isMonthly ? monthlyStatus : weeklyStatus}
         </>
     );
 }
