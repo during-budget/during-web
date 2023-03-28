e9ba935458c791dada069a87b698cf281dbd54ed
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-28 13:20:15 +0900

    feat: Calendar / Add data & blur #195

--- client/src/components/UI/Calendar.tsx
+++ client/src/components/UI/Calendar.tsx
@@ -4,16 +4,18 @@ import localeData from 'dayjs/plugin/localeData';
 import 'dayjs/locale/ko';
 import 'dayjs/locale/en';
 import classes from './Calendar.module.css';
-import { getMonthsOfWeek } from '../../util/date';
+import { getMonthsOfWeek, isToday } from '../../util/date';
 
 dayjs.extend(localeData);
 
 function Calendar(props: {
     startDate: Date;
     endDate: Date;
+    data?: any;
     isMonthTop?: boolean;
     weekIdx?: number;
     locale?: string;
+    blurAfterToday?: boolean;
 }) {
     const { startDate, endDate, isMonthTop, weekIdx, locale } = props;
     const [dates, setDates] = useState<Dayjs[]>([]);
@@ -97,14 +99,33 @@ function Calendar(props: {
         return <tr key={i + '-month'}>{tds}</tr>;
     };
 
+    const getTdClass = (day: Dayjs | null) => {
+        const target = dayjs(day?.format('YYYY-MM-DD'));
+        const today = dayjs(dayjs().format('YYYY-MM-DD'));
+
+        if (props.blurAfterToday && target && target > today) {
+            return classes.after;
+        } else {
+            return '';
+        }
+    };
+
     const weekTr = weeks.map((week, i) => {
         return (
             <Fragment key={i}>
                 {!isMonthTop && getMonthTr(week, i)}
                 <tr key={i}>
                     {week.map((day, i) => (
-                        <td key={i} className={classes.date}>
-                            {day?.format('D')}
+                        <td
+                            key={i}
+                            className={`${classes.date} ${getTdClass(day)}`}
+                        >
+                            <span className={isToday(day) ? classes.today : ''}>
+                                {day?.format('D')}
+                            </span>
+                            {day &&
+                                props.data &&
+                                props.data[day.format('YYYY-MM-DD')]}
                         </td>
                     ))}
                 </tr>
