cd89e5b65957fde50ddf6695b40c38f0a3267f16
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-29 14:21:46 +0900

    feat: Amount / Create IncomExpenseAmount component #195

---
+++ client/src/components/Budget/Amount/IncomeExpenseAmount.tsx
@@ -0,0 +1,33 @@
+import classes from './IncomeExpenseAmount.module.css';
+
+function IncomeExpenseAmount(props: {
+    expense: number;
+    income: number;
+    showZero?: boolean;
+    showEmpty?: boolean;
+}) {
+    const { expense, income } = props;
+
+    const show = props.showEmpty || expense || income || null;
+
+    return (
+        <div className={classes.container} style={{ opacity: show ? 1 : 0 }}>
+            {expense || props.showZero ? (
+                <p className={classes.expense}>
+                    {'-' + expense.toLocaleString()}
+                </p>
+            ) : (
+                ''
+            )}
+            {income || props.showZero ? (
+                <p className={classes.income}>
+                    {'+' + income.toLocaleString()}
+                </p>
+            ) : (
+                ''
+            )}
+        </div>
+    );
+}
+
+export default IncomeExpenseAmount;
