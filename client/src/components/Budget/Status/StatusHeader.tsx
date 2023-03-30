0327e84c58c81cf0dc48da43c09927b2efabbb41
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-30 14:14:49 +0900

    refactor: StatusHeader / Use ExpenseTab props #25

--- client/src/components/Budget/Status/StatusHeader.tsx
+++ client/src/components/Budget/Status/StatusHeader.tsx
@@ -1,11 +1,18 @@
 import RadioTab from '../../UI/RadioTab';
 import classes from './StatusHeader.module.css';
 
-function StatusHeader(props: { id: string; title: string; values: any; className?: string }) {
+function StatusHeader(props: {
+    id: string;
+    title: string;
+    values?: any;
+    tab?: any;
+    className?: string;
+}) {
     return (
         <div className={`${classes.container} ${props.className}`}>
             <h5>{props.title}</h5>
-            <RadioTab name={props.id} values={props.values} />
+            {props.values && <RadioTab name={props.id} values={props.values} />}
+            {props.tab}
         </div>
     );
 }
