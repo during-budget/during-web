5e9e12ab89c6fd61048eef6635373ad56f2a7f2d
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-30 17:27:51 +0900

    feat: CategoryPlan / Create category + plan input list #25

--- client/src/components/Budget/Category/CategoryPlan.tsx
+++ client/src/components/Budget/Category/CategoryPlan.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import classes from './CategoryPlan.module.css';
 import Category from '../../../models/Category';
 import { uiActions } from '../../../store/ui';
 import CompleteCancelButtons from '../../UI/CompleteCancelButtons';
 import Overlay from '../../UI/Overlay';
 import StatusHeader from '../Status/StatusHeader';
 import ExpenseTab from '../UI/ExpenseTab';
+import CategoryPlanItem from './CategoryPlanItem';
 
 function CategoryPlan(props: { title: string; categories: Category[] }) {
     const dispatch = useDispatch();
@@ -29,6 +31,7 @@ function CategoryPlan(props: { title: string; categories: Category[] }) {
 
     return (
         <Overlay
+            className={`${classes.container} ${isOpen ? classes.open : ''}`}
             isOpen={isOpen}
             isShowBackdrop={true}
             closeHandler={closeHandler}
@@ -45,6 +48,20 @@ function CategoryPlan(props: { title: string; categories: Category[] }) {
                     />
                 }
             />
+            <ul className={classes.list}>
+                <h6>목표 예산</h6>
+                <div>
+                    {categories.map((item, i) => (
+                        <CategoryPlanItem
+                            item={item}
+                            key={i}
+                            icon={item.icon}
+                            title={item.title}
+                            plan={item.amount?.planned}
+                        />
+                    ))}
+                </div>
+            </ul>
             <CompleteCancelButtons onClose={closeHandler} />
         </Overlay>
     );
