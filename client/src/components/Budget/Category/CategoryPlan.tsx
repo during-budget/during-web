e05d925e7038cad736bf53f08b599b91a1ae4870
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-30 17:26:18 +0900

    feat: CategoryPlan / Get categories from budget component #25

--- client/src/components/Budget/Category/CategoryPlan.tsx
+++ client/src/components/Budget/Category/CategoryPlan.tsx
@@ -1,16 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Category from '../../../models/Category';
 import { uiActions } from '../../../store/ui';
 import CompleteCancelButtons from '../../UI/CompleteCancelButtons';
 import Overlay from '../../UI/Overlay';
 import StatusHeader from '../Status/StatusHeader';
 import ExpenseTab from '../UI/ExpenseTab';
 
-function CategoryPlan() {
+function CategoryPlan(props: { title: string; categories: Category[] }) {
     const dispatch = useDispatch();
+
     const isOpen = useSelector((state: any) => state.ui.category.isOpen);
 
     const [isExpense, setIsExpense] = useState(true);
+    const [categories, setCategories] = useState<Category[]>([]);
+
+    useEffect(() => {
+        setCategories(
+            props.categories.filter(
+                (item: Category) => item.isExpense === isExpense
+            )
+        );
+    }, [props.categories, isExpense]);
 
     const closeHandler = () => {
         dispatch(uiActions.showCategory(false));
@@ -24,7 +35,8 @@ function CategoryPlan() {
         >
             <StatusHeader
                 id="category-plan-type"
-                title="카테고리별 현황"
+                className={classes.header}
+                title={`${props.title} 카테고리별 목표`}
                 tab={
                     <ExpenseTab
                         id="category-plan-type-tab"
