25fa138e7c4fda31928226a799858f28fab2918a
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-03-31 21:09:52 +0900

    refactor: AmountDetail / Use EditInput #25

--- client/src/components/Budget/Amount/AmountDetailItem.tsx
+++ client/src/components/Budget/Amount/AmountDetailItem.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react';
 import InputField from '../../UI/InputField';
+import EditInput from '../Input/EditInput';
 import classes from './AmountDetailItem.module.css';
 
 function AmountDetailItem(props: {
@@ -9,54 +10,31 @@ function AmountDetailItem(props: {
     labelColor: string;
     fontColor: string;
     fontSize: string;
-    editHandler?: (amount: number) => void;
+    editHandler?: (amount: string) => void;
     editDefaultValue?: number;
 }) {
-    const [isEdit, setIsEdit] = useState(false);
-    const editRef = useRef<HTMLInputElement>(null);
-
     const style = {
         '--label-color': props.labelColor,
         '--font-color': props.fontColor,
         '--font-size': props.fontSize,
     } as React.CSSProperties;
 
-    const editHandler = () => {
-        if (isEdit) {
-            const amount = editRef.current?.value || 0;
-            props.editHandler && props.editHandler(+amount);
-        }
-
-        setIsEdit((prev) => !prev);
+    const convertToAmountNum = (value: string) => {
+        return value.replace(/[^0-9]/g, '');
     };
-
-    const defaultAmount = +props.amountStr.replace(/[^0-9]/g, '');
-
-    const editInput = (
-        <input
-            className={classes.edit}
-            ref={editRef}
-            type="number"
-            defaultValue={defaultAmount}
-        ></input>
-    );
-
-    const amountSpan = (
-        <span className={classes.amount}>{props.amountStr}</span>
-    );
 
     return (
         <li className={`${classes.container} ${props.className}`} style={style}>
             <span className={classes.label}>{props.label}</span>
-            {isEdit ? editInput : amountSpan}
-            {props.editHandler && (
-                <button
-                    type="button"
-                    className={`${classes.edit} ${
-                        isEdit ? classes.check : classes.pencil
-                    }`}
-                    onClick={editHandler}
-                ></button>
+            {props.editHandler ? (
+                <EditInput
+                    className={classes.edit}
+                    value={props.amountStr}
+                    confirmHandler={props.editHandler}
+                    convertDefaultValue={convertToAmountNum}
+                />
+            ) : (
+                <span className={classes.amount}>{props.amountStr}</span>
             )}
         </li>
     );
