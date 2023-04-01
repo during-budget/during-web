31afbaaa0fb8807072f33f27e0783433d540360d
jessie129j@gmail.com <jessie129j@gmail.com>
2023-04-01 21:53:37 +0900

    feat: Create update single category amountPlanned API #215

--- server/routes/budgets.ts
+++ server/routes/budgets.ts
@@ -7,6 +7,11 @@ router.post("/", isLoggedIn, budgets.create);
 router.post("/basic", isLoggedIn, budgets.createWithBasic);
 
 router.put("/:_id/categories", isLoggedIn, budgets.updateCategoriesV2);
+router.put(
+  "/:_id/categories/:categoryId/amountPlanned",
+  isLoggedIn,
+  budgets.updateCategoryAmountPlanned
+);
 
 router.patch("/:_id", isLoggedIn, budgets.updateField);
 
