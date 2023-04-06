2bef64579109e3f9be9e62371b9910c77a198a72
jessie129j@gmail.com <jessie129j@gmail.com>
2023-04-06 15:28:28 +0900

    feat: Create transaction update API v2 #223

--- server/routes/transactions.ts
+++ server/routes/transactions.ts
@@ -5,6 +5,8 @@ import * as transactions from "../controllers/transactions";
 
 router.post("/", isLoggedIn, transactions.create);
 
+router.put("/:_id", isLoggedIn, transactions.updateV2);
+
 router.put("/:_id/category", isLoggedIn, transactions.updateCategory);
 router.put("/:_id/amount", isLoggedIn, transactions.updateAmount);
 router.patch("/:_id", isLoggedIn, transactions.updateField);
