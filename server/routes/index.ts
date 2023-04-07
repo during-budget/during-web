e60a15b62f8ddc9d7185aba0ca4f3b7a42f2e824
jessie129j@gmail.com <jessie129j@gmail.com>
2023-04-07 22:55:12 +0900

    feat: Create paymentMethod APIs #227

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,3 +1,12 @@
-const routers = ["test", "users", "budgets", "transactions", "categories"];
+const routers = [
+  "test",
+  "users",
+  "budgets",
+  "transactions",
+  "categories",
+  "assets",
+  "cards",
+  "paymentMethods",
+];
 
 export { routers };
