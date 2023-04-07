0815f0127a4d30a1c9731d791cdccf9461805038
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-04-07 22:29:30 +0900

    feat: BasicBudget / Set NavButton #112

--- client/src/components/User/Basic/BasicBudget.tsx
+++ client/src/components/User/Basic/BasicBudget.tsx
@@ -5,9 +5,14 @@ import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { uiActions } from '../../../store/ui';
+import NavButton from '../../UI/NavButton';
+import { useLocation } from 'react-router';
 
 function BasicBudget() {
     const dispatch = useDispatch();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname;
 
     const id = useSelector((state: any) => state.user.basicBudgetId);
 
@@ -17,6 +22,11 @@ function BasicBudget() {
 
     return (
         <main className={classes.container}>
+            <NavButton
+                className={classes.back}
+                to={from || '/user'}
+                isNext={false}
+            />
             <section></section>
             <hr />
             <section>
