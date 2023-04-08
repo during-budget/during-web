7e0b998914cac1f932a71e63118739ea7a0566a0
jessie129j@gmail.com <jessie129j@gmail.com>
2023-04-08 13:46:35 +0900

    feat: Remove deprecated APIs #227

--- server/routes/transactions.ts
+++ server/routes/transactions.ts
@@ -7,10 +7,6 @@ router.post("/", isLoggedIn, transactions.create);
 
 router.put("/:_id", isLoggedIn, transactions.updateV2);
 
-router.put("/:_id/category", isLoggedIn, transactions.updateCategory);
-router.put("/:_id/amount", isLoggedIn, transactions.updateAmount);
-router.patch("/:_id", isLoggedIn, transactions.updateField);
-
 router.get("/:_id?", isLoggedIn, transactions.find);
 
 router.delete("/:_id", isLoggedIn, transactions.remove);
