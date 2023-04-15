93166e558330b55d9c99cc4422457b420fa45b4e
jessie129j@gmail.com <jessie129j@gmail.com>
2023-04-15 14:13:46 +0900

    fix: Fix index.ts and email.ts #231

--- server/index.ts
+++ server/index.ts
@@ -54,17 +54,17 @@ app.use(
       secure: false, // HTTPS 통신 외에서는 쿠키를 전달하지 않는다.
     },
     rolling: true,
-    // store: new RedisStore({
-    //   client,
-    //   ttl: 24 * 60 * 60, //1 day
-    //   // no need to set reapInterval
-    // }),
-    store: new FileStore({
-      ttl: 24 * 60 * 60, // 1 day
-      // ttl: 10, // 10 secs
-      path: "./sessions",
-      reapInterval: 12 * 60 * 60, // purge all expired cookies every 12 hours
+    store: new RedisStore({
+      client,
+      ttl: 24 * 60 * 60, //1 day
+      // no need to set reapInterval
     }),
+    // store: new FileStore({
+    //   ttl: 24 * 60 * 60, // 1 day
+    //   // ttl: 10, // 10 secs
+    //   path: "./sessions",
+    //   reapInterval: 12 * 60 * 60, // purge all expired cookies every 12 hours
+    // }),
   })
 );
 app.use(passport.initialize());
