507663e3705b710f9dda44f87fdeecd21ea65d60
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-04-17 23:16:43 +0900

    feat: DefaultBudget / Create budget from default data #112

--- client/src/layout/BudgetNavigation.tsx
+++ client/src/layout/BudgetNavigation.tsx
@@ -1,16 +1,45 @@
 import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '../hooks/redux-hook';
 import Budget from '../models/Budget';
+import { createBudgetFromBasic } from '../util/api/budgetAPI';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { budgetActions } from '../store/budget';
 
 function BudgetNavigation() {
-    const budgets = useAppSelector((state) => state.budget);
-    const id = getCurrentBudgetId(budgets);
+    const dispatch = useDispatch();
 
-    return <Navigate to={`/budget/${id}`} replace={true} />;
+    const [id, setId] = useState('');
+    const budgets = useAppSelector((state) => state.budget);
+
+    useEffect(() => {
+        const getId = async () => {
+            const id = await getCurrentBudgetId(budgets);
+
+            if (id) {
+                setId(id);
+            } else {
+                const now = new Date();
+                const { budget } = await createBudgetFromBasic(
+                    now.getFullYear(),
+                    now.getMonth() + 1
+                );
+                dispatch(budgetActions.setBudget(budget));
+                setId(budget._id);
+            }
+        };
+        getId();
+    }, []);
+
+    if (id) {
+        return <Navigate to={`/budget/${id}`} replace={true} />;
+    } else {
+        return <></>;
+    }
 }
 
-const getCurrentBudgetId = (budgetObj: { [id: string]: Budget }) => {
-    let id = 'new';
+const getCurrentBudgetId = async (budgetObj: { [id: string]: Budget }) => {
+    let id;
     const now = new Date();
 
     const budgets = Object.values(budgetObj);
@@ -22,11 +51,10 @@ const getCurrentBudgetId = (budgetObj: { [id: string]: Budget }) => {
 
         const isCurrentBudget = start < now && now < nextStart;
         if (isCurrentBudget) {
-            id = budget.id; // TODO: 원래 _id로 접근해서 잘 돌아가던 코드..... 문제가 있는지 확인 필요
+            id = budget.id;
             return false;
         }
     });
-
     return id;
 };
 
