d41337cac0d920255a192132794f9d0cb7fbf347
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-04-22 15:32:34 +0900

    feat: DefaultBudget / Add isDefault parms & Use in Budget component #240

--- client/src/components/Budget/UI/BudgetHeader.tsx
+++ client/src/components/Budget/UI/BudgetHeader.tsx
@@ -1,25 +1,47 @@
+import { useLocation } from 'react-router';
 import { useAppSelector } from '../../../hooks/redux-hook';
 import { getNumericDotDateString } from '../../../util/date';
 import NavButton from '../../UI/NavButton';
 import classes from './BudgetHeader.module.css';
 
-function BudgetHeader(props: { budgetId: string }) {
+function BudgetHeader(props: { budgetId: string; isDefault?: boolean }) {
+  const { budgetId, isDefault } = props;
+
   // get budget Data
   const budgets = useAppSelector((state) => state.budget);
-  const { title, date } = budgets[props.budgetId];
+  const { title, date } = budgets[budgetId];
+
+  // get location
+  const location = useLocation();
+  const from = location.state?.from?.pathname;
+
+  // set button
+  const defaultButton = (
+    <NavButton className={classes.back} to={from || '/user'} isNext={false} />
+  );
 
   // TODO: BudgetList 구현
-  // TODO: Search 구현
+  const hamburgerButton = (
+    <button type="button">
+      <i className="fa-solid fa-bars"></i>
+    </button>
+  );
+
+  // title
+  const defaultHeader = <span>매월 반복</span>;
+  const dateHeader = (
+    <>
+      <span>{getNumericDotDateString(date.start)}</span>
+      <span>{getNumericDotDateString(date.end)}</span>
+      <span> ~ </span>
+    </>
+  );
+
   const top = (
     <>
-      <button type="button">
-        <i className="fa-solid fa-bars"></i>
-      </button>
-      <div className={classes.date}>
-        <span>{getNumericDotDateString(date.start)}</span>
-        <span> ~ </span>
-        <span>{getNumericDotDateString(date.end)}</span>
-      </div>
+      {isDefault ? defaultButton : hamburgerButton}
+      <div className={classes.date}>{isDefault ? defaultHeader : dateHeader}</div>
+      {/* TODO: Search 구현 */}
       <button type="button">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
@@ -27,7 +49,9 @@ function BudgetHeader(props: { budgetId: string }) {
   );
 
   // TODO: 이전/이후 budget id 가져오기 - 이동 구현
-  const bottom = (
+  const bottom = isDefault ? (
+    <h1>{title}</h1>
+  ) : (
     <>
       <NavButton to="/budget" isNext={false} />
       <h1>{title}</h1>
