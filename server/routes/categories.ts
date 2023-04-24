3f1e7d27906337b0aadf03b398f3b2fae3f9c8e9
jessie129j@gmail.com <jessie129j@gmail.com>
2023-04-24 13:20:11 +0900

    feat: Create update user categories partially API #247

--- server/routes/categories.ts
+++ server/routes/categories.ts
@@ -6,6 +6,8 @@ import * as categories from "../controllers/categories";
 
 router.put("/", isLoggedIn, categories.updateV2);
 
+router.patch("/", isLoggedIn, categories.updateV3);
+
 router.get("/", isLoggedIn, categories.find);
 
 module.exports = router;
