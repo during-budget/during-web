16ebe45e842d484e1bed57f13dc9a89f1e134159
jessie129j@gmail.com <jessie129j@gmail.com>
2023-04-26 16:14:02 +0900

    feat: Update production logger #252

--- server/log/prodLogger.ts
+++ server/log/prodLogger.ts
@@ -4,20 +4,25 @@ import WinstonDaily from "winston-daily-rotate-file";
 import * as _S3StreamLogger from "s3-streamlogger-daily";
 const S3StreamLogger = _S3StreamLogger.S3StreamLogger;
 import strftime from "strftime";
+import mongoose from "mongoose";
 
 const strftimeKOR = strftime.timezone("+0900");
 const time_data = () => strftimeKOR("%F", new Date());
 
-
 /* prodLogger */
 const stream = (level = "") => {
+  const instanceId = new mongoose.Types.ObjectId();
   const time_data = strftimeKOR("%F %T", new Date());
+  console.log("instanceId: ", instanceId);
 
   return new S3StreamLogger({
     bucket: process.env.S3_BUCKET_LOGS ?? "undefined",
+    folder: "raw",
     access_key_id: process.env.S3_ACESSKEYID ?? "undefined",
     secret_access_key: process.env.S3_SECRETACCESSKEY ?? "undefined",
-    name_format: `${time_data}${level !== "" ? "." + level : ""}.log`,
+    name_format: `${time_data} ${instanceId}${
+      level !== "" ? "." + level : ""
+    }.log`,
     rotate_every: "day",
     max_file_size: 2000000000, // 2gb
   });
