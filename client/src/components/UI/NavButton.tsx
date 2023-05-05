7a255babe8de7764e7d635ba2ced483c989d2330
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-05-05 14:28:59 +0900

    feat: UI / Add StepNav & Update NavButton #283

--- client/src/components/UI/NavButton.tsx
+++ client/src/components/UI/NavButton.tsx
@@ -1,27 +1,30 @@
 import { Link } from 'react-router-dom';
 
 // NOTE: props to가 있을 경우 Link, onClick이 있을 경우 Button, 둘 다 있을 경우 Link
-function NavButton(props: {
-    to?: string;
-    onClick?: (event: React.MouseEvent) => void;
-    isNext?: boolean;
-    className?: string;
-}) {
-    const chevron = props.isNext ? '>' : '<';
-
-    const navLink = props.to && (
-        <Link className={props.className} to={props.to} onClick={props.onClick}>
-            {chevron}
-        </Link>
-    );
-
-    const navButton = props.onClick && (
-        <button type="button" onClick={props.onClick}>
-            {chevron}
-        </button>
-    );
-
-    return navLink || navButton || <></>;
+interface NavButtonProps {
+  to?: string;
+  onClick?: (event: React.MouseEvent) => void;
+  isNext?: boolean;
+  className?: string;
+  content?: string;
 }
+
+const NavButton = ({ to, onClick, isNext, className, content }: NavButtonProps) => {
+  const chevron = isNext ? '>' : '<';
+
+  const navLink = to && (
+    <Link className={className} to={to} onClick={onClick}>
+      {chevron}
+    </Link>
+  );
+
+  const navButton = onClick && (
+    <button className={className} type="button" onClick={onClick}>
+      {content ? content : chevron}
+    </button>
+  );
+
+  return navLink || navButton || <></>;
+};
 
 export default NavButton;
