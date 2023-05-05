a3a606c450a121104b70239e0fbb1c8b00f0ca38
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-05-05 13:41:50 +0900

    feat: UI / Create Inform component #283

---
+++ client/src/components/UI/Inform.tsx
@@ -0,0 +1,28 @@
+import { PropsWithChildren } from 'react';
+import classes from './Inform.module.css';
+
+interface InfomProps {
+  isError: boolean;
+  className?: string;
+  hideIcon?: boolean;
+  isThin?: boolean;
+}
+
+const Inform = ({
+  isError,
+  className,
+  hideIcon,
+  isThin,
+  children,
+}: PropsWithChildren<InfomProps>) => {
+  const msgClass = isError ? classes.error : classes.inform;
+  const thinClass = isThin ? classes.thin : '';
+  return (
+    <div className={`${classes.msg} ${msgClass} ${thinClass} ${className}`}>
+      {!hideIcon && <i className={`fa-solid fa-circle-exclamation ${classes.icon}`} />}
+      {children}
+    </div>
+  );
+};
+
+export default Inform;
