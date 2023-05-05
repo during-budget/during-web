581f55b772877f024a6ae49039bc09fed82922dc
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-05-05 14:28:59 +0900

    feat: UI / Add StepNav & Update NavButton #283

---
+++ client/src/components/UI/StepNav.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import NavButton from './NavButton';
+import classes from './StepNav.module.css';
+
+interface StepNavProps {
+  idx: number;
+  setIdx: React.Dispatch<React.SetStateAction<number>>;
+  length: number;
+  onComplete?: () => void;
+  className?: string;
+}
+
+const StepNav = ({ idx, setIdx, length, onComplete, className }: StepNavProps) => {
+  const isComplete = idx === length - 1;
+  const completeClass = isComplete ? classes.complete : '';
+  const completeContent = isComplete ? '✓' : undefined;
+
+  const hideClass = idx === 0 ? classes.hide : '';
+
+  const goPrev = () => {
+    if (idx > 0) {
+      setIdx(idx - 1);
+    }
+  };
+
+  const goNext = () => {
+    if (isComplete) {
+      onComplete && onComplete();
+    } else {
+      setIdx(idx + 1);
+    }
+  };
+
+  return (
+    <nav className={`${classes.nav} ${className}`}>
+      <NavButton className={`${classes.prev} ${hideClass}`} onClick={goPrev} />
+      <div>
+        <span className={classes.idx}>{idx + 1}</span> / {length}
+      </div>
+      <NavButton
+        className={`${classes.next} ${completeClass}`}
+        onClick={goNext}
+        isNext={true}
+        content={completeContent}
+      />
+    </nav>
+  );
+};
+
+export default StepNav;
