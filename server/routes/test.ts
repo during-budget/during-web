00380f3834e5806dc36a7476159bd9f48784c802
jessie129j@gmail.com <jessie129j@gmail.com>
2023-05-06 12:05:34 +0900

    fix: Remove test APIs #276

--- server/routes/test.ts
+++ server/routes/test.ts
@@ -1,30 +1,8 @@
 import express, { Request, Response } from "express";
 const router = express.Router();
-import * as test from "../controllers/test";
-import * as budgets from "../controllers/budgets";
-import * as transactions from "../controllers/transactions";
 
 router.get("/", async (req: Request, res: Response) => {
   return res.status(200).send({ message: "hello world!" });
 });
-
-/**
- */
-router.get("/:model", test.findDocuments);
-router.get("/:model/:_id", test.findDocument);
-
-router.delete("/users/:_id", test.removeUser);
-router.delete("/budgets/:_id", budgets.remove);
-router.delete("/transactions/:_id", transactions.remove);
-
-router.get("/", test.hello);
-router.get("/dataList", test.dataList);
-/**
- * CRUD testData
- */
-router.post("/", test.create);
-router.get("/:_id", test.find);
-router.put("/:_id", test.update);
-router.delete("/:_id", test.remove);
 
 module.exports = router;
