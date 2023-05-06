59e3fbabd9bf276429e995ebd516436fa43d862a
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-05-06 20:31:57 +0900

    feat: Sentry / Init Sentry #305

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,22 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store/index';
+import * as Sentry from '@sentry/react';
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
+if (import.meta.env.MODE === 'production') {
+  Sentry.init({
+    dsn: 'https://7447449226b942609c7f55f66d10d401@o4505136726605824.ingest.sentry.io/4505136729227264',
+    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
