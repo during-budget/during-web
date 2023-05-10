2c0d8c46971f97a43165512f0be77efced1e7fe3
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-05-10 13:09:40 +0900

    feat: Asset / Add asset or card button #326

--- client/src/components/Asset/Status/AssetCardStatus.tsx
+++ client/src/components/Asset/Status/AssetCardStatus.tsx
@@ -1,14 +1,25 @@
 import { useState } from 'react';
 import { AssetProps } from '../../../screens/Asset';
 import Carousel from '../../UI/Carousel';
+import AssetCardEditor from '../Editor/AssetCardEditor';
 import AssetCardListEditor from '../Editor/AssetCardListEditor';
 import classes from './AssetCardStatus.module.css';
 import AssetStatus from './AssetStatus';
 import CardStatus from './CardStatus';
 
 const AssetCardStatus = ({ assets, cards }: AssetProps) => {
+  const [showListEditor, setShowListEditor] = useState(false);
   const [showEditor, setShowEditor] = useState(false);
   const [isAssetEditor, setIsAssetEditor] = useState(true);
+
+  const openListEditor = (payload: { isAsset: boolean }) => {
+    setIsAssetEditor(payload.isAsset);
+    setShowListEditor(true);
+  };
+
+  const closeListEditor = () => {
+    setShowListEditor(false);
+  };
 
   const openEditor = (payload: { isAsset: boolean }) => {
     setIsAssetEditor(payload.isAsset);
@@ -22,13 +33,27 @@ const AssetCardStatus = ({ assets, cards }: AssetProps) => {
   return (
     <>
       <Carousel id="asset-payment-status" itemClassName={classes.status}>
-        <AssetStatus assets={assets} openEditor={openEditor} />
-        <CardStatus assets={assets} cards={cards} openEditor={openEditor} />
+        <AssetStatus
+          assets={assets}
+          openListEditor={openListEditor}
+          openEditor={openEditor}
+        />
+        <CardStatus
+          assets={assets}
+          cards={cards}
+          openListEditor={openListEditor}
+          openEditor={openEditor}
+        />
       </Carousel>
       <AssetCardListEditor
+        isOpen={showListEditor}
+        closeEditor={closeListEditor}
+        isAsset={isAssetEditor}
+      />
+      <AssetCardEditor
+        isAsset={isAssetEditor}
         isOpen={showEditor}
         closeEditor={closeEditor}
-        isAsset={isAssetEditor}
       />
     </>
   );
