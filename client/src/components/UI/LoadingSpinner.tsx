4c6c186be58b3d207fbbd62c7643dbcb2dfba916
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-05-13 10:07:16 +0900

    feat: Loading / Show loading spinner component on first auth screen load #332

---
+++ client/src/components/UI/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+import classes from './LoadingSpinner.module.css'
+
+const LoadingSpinner = () => {
+  return (
+    <div className={classes.loading}>Loading...</div>
+  )
+}
+
+export default LoadingSpinner
