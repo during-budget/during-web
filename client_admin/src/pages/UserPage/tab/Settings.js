a0b2eebdd3f017d45455ec8d8da49dbd8e5d3c2b
jessie129j@gmail.com <jessie129j@gmail.com>
2023-05-13 14:17:04 +0900

    feat: Update admin pages #335

---
+++ client_admin/src/pages/UserPage/tab/Settings.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import useAPI from "../../../hooks/useAPI";
+
+import Detail from "../../../components/Detail";
+import Loading from "../../../components/Loading";
+
+function Index() {
+  const API = useAPI();
+  const { _id } = useParams();
+  const [settings, setSettings] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+
+  const updateData = async () => {
+    const { user } = await API.GET({ location: "users/" + _id });
+    setSettings(user?.settings ?? {});
+  };
+
+  useEffect(() => {
+    if (isLoading) {
+      updateData()
+        .then(() => {
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          return navigate("/users");
+        });
+    }
+    return () => {};
+  }, [isLoading]);
+
+  return !isLoading ? (
+    Detail(settings, { marginLeft: "0px", marginRight: "0px" })
+  ) : (
+    <Loading />
+  );
+}
+
+export default Index;
