dc5329333c28195ef86c18afd043ba46d6124fb5
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-05-17 12:42:53 +0900

    feat: Auth / Update style for sns login #358

--- client/src/components/Auth/EmailForm.tsx
+++ client/src/components/Auth/EmailForm.tsx
@@ -1,153 +1,135 @@
-import { useRef, useState } from 'react';
-import classes from './EmailForm.module.css';
-import InputField from '../UI/InputField';
-import Button from '../UI/Button';
-import CodeField from '../Auth/CodeField';
-import { throwError } from '../../util/error';
+import { useEffect, useRef, useState } from 'react';
 import {
-    sendCodeLogin,
-    sendCodeRegister,
-    verifyLogin,
-    verifyRegister,
+  sendCodeLogin,
+  sendCodeRegister,
+  verifyLogin,
+  verifyRegister,
 } from '../../util/api/userAPI';
-import { getAuthURL } from '../../util/api/snsIdAPI';
-import { GoogleLoginButton, KakaoLoginButton, NaverLoginButton } from './SocialLoginButton';
+import { throwError } from '../../util/error';
+import CodeField from '../Auth/CodeField';
+import Button from '../UI/Button';
+import InputField from '../UI/InputField';
+import classes from './EmailForm.module.css';
 
 function EmailForm(props: {
-    isLogin: boolean;
-    toggleIsLogin: () => void;
-    changeAuthType: () => void;
-    getUserLogin: (user: any) => void;
+  isLogin: boolean;
+  toggleIsLogin: () => void;
+  changeAuthType: () => void;
+  getUserLogin: (user: any) => void;
 }) {
-    const { isLogin, toggleIsLogin } = props;
+  const { isLogin, toggleIsLogin } = props;
 
-    const [emailState, setEmailState] = useState('');
-    const [emailVerifyState, setEmailVerifyState] = useState(false);
-    const codeRef = useRef<any>();
+  const [emailState, setEmailState] = useState('');
+  const [emailVerifyState, setEmailVerifyState] = useState(false);
+  const codeRef = useRef<any>();
 
-    // Handlers
-    const sendHandler = async (event?: React.MouseEvent) => {
-        event!.preventDefault();
+  // Handlers
+  const sendHandler = async (event?: React.MouseEvent) => {
+    event!.preventDefault();
 
-        try {
-            if (isLogin) {
-                await sendCodeLogin(emailState);
-            } else {
-                await sendCodeRegister(emailState);
-            }
+    try {
+      if (isLogin) {
+        await sendCodeLogin(emailState);
+      } else {
+        await sendCodeRegister(emailState);
+      }
 
-            setEmailVerifyState(true);
-        } catch (error) {
-            throwError(error);
-        }
-    };
+      setEmailVerifyState(true);
+    } catch (error) {
+      throwError(error);
+    }
+  };
 
-    const verifyHandler = async (event?: React.MouseEvent) => {
-        event!.preventDefault();
+  const verifyHandler = async (event?: React.MouseEvent) => {
+    event!.preventDefault();
 
-        let data;
-        try {
-            const code = codeRef.current!.value();
+    let data;
+    try {
+      const code = codeRef.current!.value();
 
-            // TODO: 자동로그인(persist) 체크박스 추가
-            const persist = true;
+      // TODO: 자동로그인(persist) 체크박스 추가
+      const persist = true;
 
-            if (isLogin) {
-                data = await verifyLogin(emailState, code, persist);
-            } else {
-                data = await verifyRegister(emailState, code, persist);
-            }
+      if (isLogin) {
+        data = await verifyLogin(emailState, code, persist);
+      } else {
+        data = await verifyRegister(emailState, code, persist);
+      }
 
-            // TODO: 로그인 처리 & 카테고리 및 budget 처리
-            props.getUserLogin(data.user);
-        } catch (error) {
-            throwError(error);
-        }
-    };
+      props.getUserLogin(data.user);
+    } catch (error) {
+      throwError(error);
+    }
+  };
 
-    const emailHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setEmailState(event.target.value);
-    };
+  const emailHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmailState(event.target.value);
+  };
 
-    return (
-        <div className={classes.container}>
-            <img src="/assets/png/logo.png" alt="듀링 가계부 로고" />
-            <h2>{isLogin ? '로그인' : '회원가입'}</h2>
-            <form className={classes.form}>
-                <InputField
-                    id="auth-email-field"
-                    className={`${classes.field} ${classes.emailField}`}
-                >
-                    <p className={classes.emailLabel}>
-                        <label htmlFor="register-email">이메일</label>
-                        {emailVerifyState && (
-                            <Button sizeClass="sm" onClick={sendHandler}>
-                                재전송
-                            </Button>
-                        )}
-                    </p>
-                    <input
-                        id="auth-email"
-                        type="email"
-                        value={emailState}
-                        onChange={emailHandler}
-                        required
-                    />
-                </InputField>
-                {emailVerifyState ? (
-                    <>
-                        <CodeField className={classes.code} ref={codeRef} />
-                        <Button
-                            type="submit"
-                            className={classes.submit}
-                            onClick={verifyHandler}
-                        >
-                            {isLogin ? '로그인' : '회원가입'}
-                        </Button>
-                    </>
-                ) : (
-                    <Button
-                        type="submit"
-                        className={classes.submit}
-                        onClick={sendHandler}
-                    >
-                        {isLogin
-                            ? '로그인 인증코드 전송'
-                            : '회원가입 인증코드 전송'}
-                    </Button>
-                )}
-            </form>
-            <div className={classes.buttons}>
-                <Button
-                    styleClass="extra"
-                    onClick={() => {
-                        toggleIsLogin();
-                    }}
-                >
-                    {isLogin ? '회원가입' : '로그인'}
-                </Button>
-                |{' '}
-                <Button styleClass="extra" onClick={props.changeAuthType}>
-                    {isLogin ? 'SNS로 로그인' : 'SNS로 회원가입'}
-                </Button>
-            </div>
-            <div style={{display:"flex", flexDirection:"column", gap:"12px"}}>
-                <div>{"--------------------------------"}</div>
-                <div>
-                {`주석: 소셜 로그인 시 계정이 연결된 경우에는 로그인을 진행하고, 계정이 없는 경우에는 자동으로 회원가입 이 이루어진 후 로그인됩니다.`}
-            </div>
-                <a href={getAuthURL('google')}>
-                    <GoogleLoginButton />
-                </a>
-                <a href={getAuthURL('naver')}>
-                    <NaverLoginButton />
-                </a>
-                <a href={getAuthURL('kakao')}>
-                    <KakaoLoginButton />
-                </a>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    const field = document.querySelector('#auth-email-field input') as HTMLInputElement;
+    field?.focus();
+  }, []);
+
+  return (
+    <div className={classes.email}>
+      <img src="/assets/images/logo.png" alt="듀링 가계부 로고" />
+      <h2>{isLogin ? '로그인' : '회원가입'}</h2>
+      <form className={classes.form}>
+        <InputField
+          id="auth-email-field"
+          className={`${classes.field} ${classes.emailField}`}
+        >
+          <div className={classes.emailLabel}>
+            <label htmlFor="register-email">
+              이메일로 {isLogin ? '로그인' : '회원가입'}
+            </label>
+            {emailVerifyState && (
+              <Button sizeClass="sm" onClick={sendHandler}>
+                재전송
+              </Button>
+            )}
+          </div>
+          <input
+            id="auth-email"
+            type="email"
+            value={emailState}
+            onChange={emailHandler}
+            required
+          />
+        </InputField>
+        {emailVerifyState ? (
+          <>
+            <CodeField className={classes.code} ref={codeRef} />
+            <Button type="submit" className={classes.submit} onClick={verifyHandler}>
+              {isLogin ? '로그인' : '회원가입'}
+            </Button>
+          </>
+        ) : (
+          <Button type="submit" className={classes.submit} onClick={sendHandler}>
+            {isLogin ? '로그인 인증코드 전송' : '회원가입 인증코드 전송'}
+          </Button>
+        )}
+      </form>
+      <div className={classes.buttons}>
+        <Button styleClass="extra" className={classes.guest}>
+          가입 없이 둘러보기
+        </Button>
+        <span>|</span>
+        <Button
+          styleClass="extra"
+          onClick={() => {
+            toggleIsLogin();
+          }}
+        >
+          {isLogin ? '회원가입하기' : '로그인하기'}
+        </Button>
+      </div>
+      <Button styleClass="extra" className={classes.sns} onClick={props.changeAuthType}>
+        SNS로 {isLogin ? '로그인' : '회원가입'}
+      </Button>
+    </div>
+  );
 }
 
 export default EmailForm;
--- client/src/components/Auth/SocialLoginButton.tsx
+++ client/src/components/Auth/SocialLoginButton.tsx
@@ -2,38 +2,27 @@
  * @title 소셜 로그인 버튼
  * @description 각 버튼은 구글/네이버/카카오에서 제공하는 가이드라인에 따라 디자인되어야 합니다.
  */
-import React from "react";
 
-const url = `assets/images/`;
-
-const style = {
-  cursor: "pointer",
-  border: "none",
-  backgroundColor: "transparent",
-};
-
-const styleImg = {
-  width: "180px"
-};
+const url = `assets/svg/`;
 
 // guideline: https://developers.google.com/identity/branding-guidelines?hl=ko
 export const GoogleLoginButton = ({}) => (
-  <button type={"button"} style={style}>
-    <img style={styleImg}  src={url + "google.png"} alt="asdf" />
+  <button type={"button"}>
+    <img src={url + "social_icon_google.svg"} alt="Google login button" />
   </button>
 );
 
 // guideline: https://developers.naver.com/docs/login/bi/bi.md
 export const NaverLoginButton = ({}) => (
-  <button type={"button"} style={style}>
-    <img style={styleImg} src={url + "naver.png"} alt="asdf" />
+  <button type={"button"}>
+    <img src={url + "social_icon_naver.svg"} alt="Naver login button" />
   </button>
 );
 
 // resource: https://developers.kakao.com/tool/resource/login
 export const KakaoLoginButton = ({}) => (
-  <button type={"button"} style={style}>
-    <img style={styleImg} src={url + "kakao.png"} alt="asdf" />
+  <button type={"button"}>
+    <img src={url + "social_icon_kakao.svg"} alt="Kakao login button" />
   </button>
 );
 
