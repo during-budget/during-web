d3283fb2ccea365a8b69a9808f04e1371df718ad
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-05-18 15:58:49 +0900

    feat: Redirect / Add social auth redirection - show modal & redirect #365

--- client/src/screens/Auth.tsx
+++ client/src/screens/Auth.tsx
@@ -9,13 +9,11 @@ import { useAppDispatch } from '../hooks/redux-hook';
 import { assetActions } from '../store/asset';
 import { budgetActions } from '../store/budget';
 import { settingActions } from '../store/setting';
-import { uiActions } from '../store/ui';
 import { userActions } from '../store/user';
 import { userCategoryActions } from '../store/user-category';
 import { getBudgetById, getBudgetList } from '../util/api/budgetAPI';
 import { UserDataType, getUserState } from '../util/api/userAPI';
 import classes from './Auth.module.css';
-import { useSearchParams } from 'react-router-dom';
 
 function Auth() {
   const dispatch = useAppDispatch();
@@ -24,63 +22,6 @@ function Auth() {
   const [isFirstLoad, setIsFirstLoad] = useState(true);
   const [isEmailAuth, setIsEmailAuth] = useState(false);
   const [isLogin, setIsLogin] = useState(true);
-
-  // Get social login message
-  const [params] = useSearchParams();
-  const snsLoginMsg = params.get('message');
-
-  useEffect(() => {
-    switch (snsLoginMsg) {
-      case 'LOGIN_SUCCESS':
-      case 'REGISTER_SUCCESS':
-        navigate('/budget');
-        break;
-      case 'REGISTER_FAILED_EMAIL_IN_USE':
-        dispatch(
-          uiActions.showModal({
-            title: '이미 사용 중인 이메일입니다.',
-            description: '이메일로 로그인 후 소셜 계정 연결을 진행해주세요.',
-          })
-        );
-        break;
-      // case 'CONNECT_FAILED_ALREADY_CONNECTED':
-      //   dispatch(
-      //     uiActions.showModal({
-      //       title: '이미 연결된 소셜 계정입니다.',
-      //     })
-      //   );
-      //   break;
-      // case 'CONNECT_FAILED_SNSID_IN_USE':
-      //   dispatch(
-      //     uiActions.showModal({
-      //       title: '다른 계정에서 사용 중인 소셜 계정입니다.',
-      //     })
-      //   );
-      //   break;
-      // case 'CONNECT_SUCCESS':
-      //   break;
-      case 'AUTH_FAILED_UNKNOWN_ERROR':
-        dispatch(
-          uiActions.showModal({
-            title: '문제가 발생했습니다.',
-            description: '잠시 후 다시 시도해주세요.',
-            actionMsg: '제보하기',
-            actionPrefix: '문제가 계속되나요? ',
-          })
-        );
-        break;
-      case 'INVALIED_REQUEST':
-        dispatch(
-          uiActions.showModal({
-            title: '잘못된 요청입니다',
-            description: '다시 시도해주세요.',
-            actionMsg: '제보하기',
-            actionPrefix: '문제가 계속되나요? ',
-          })
-        );
-        break;
-    }
-  }, [snsLoginMsg]);
 
   // Get login
   const location = useLocation();
