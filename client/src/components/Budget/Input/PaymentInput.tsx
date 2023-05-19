0a44ef6333b94313b146affa7f82113e41851963
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-05-20 07:58:08 +0900

    feat: PaymentInput / Add tab - asset, card #351

--- client/src/components/Budget/Input/PaymentInput.tsx
+++ client/src/components/Budget/Input/PaymentInput.tsx
@@ -1,7 +1,8 @@
-import React, { useImperativeHandle, useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useImperativeHandle, useMemo, useRef, useState } from 'react';
 import { useAppSelector } from '../../../hooks/redux-hook';
+import RadioTab from '../../UI/RadioTab';
 import Select from '../../UI/Select';
+import classes from './PaymentInput.module.css';
 
 interface PaymentInputProps {
   budgetId: string;
@@ -19,20 +20,63 @@ const PaymentInput = React.forwardRef((props: PaymentInputProps, ref) => {
       value: () => paymentRef.current!.value(),
     };
   });
+
+  const [isAsset, setIsAsset] = useState(false);
+
   const paymentRef = useRef<any>(null);
 
-  const paymentMethods = useAppSelector((state) => state.asset.paymentMethods);
+  const storedPaymentMethods = useAppSelector((state) => state.asset.paymentMethods);
+  const filteredPaymentMethods = useMemo(
+    () =>
+      storedPaymentMethods.filter((item) => {
+        if (isAsset) {
+          return item.type === 'asset';
+        } else {
+          return item.type === 'card';
+        }
+      }),
+    [storedPaymentMethods, isAsset]
+  );
+
+  const paymentTab = {
+    element: (
+      <RadioTab
+        className={classes.tab}
+        key="payment-input-asset-card-tab"
+        name="payment-input-asset-card-tab"
+        values={[
+          {
+            label: '계좌/현금',
+            value: 'asset',
+            checked: isAsset,
+            onChange: () => {
+              setIsAsset(true);
+            },
+          },
+          {
+            label: '카드',
+            value: 'card',
+            checked: !isAsset,
+            onChange: () => {
+              setIsAsset(false);
+            },
+          },
+        ]}
+      />
+    ),
+  };
 
   const paymentOptions = [
-    ...paymentMethods
-      .filter((item) => item.isChecked)
+    paymentTab,
+    ...filteredPaymentMethods
+      .filter((item) => item.type === 'asset' || item.isChecked)
       .map((item) => {
         return {
           value: item._id,
           label: `${item.icon} ${item.title}`,
         };
       }),
-    { value: '', label: '결제수단 없음' },
+    { value: '결제수단 없음', label: '결제수단 없음' },
   ];
 
   return (
@@ -44,7 +88,7 @@ const PaymentInput = React.forwardRef((props: PaymentInputProps, ref) => {
         props.setIsEditSetting(true);
       }}
       onChange={(value?: string) => {
-        const payment = paymentMethods.find((item) => item._id === value);
+        const payment = filteredPaymentMethods.find((item) => item._id === value);
         props.onChange && props.onChange(value, payment?.detail === 'credit');
       }}
       value={props.value}
