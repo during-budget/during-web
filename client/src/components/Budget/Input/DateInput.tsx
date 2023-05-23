0c4977f7d69cde98c26484b71077a68a532dc1b9
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-05-23 09:49:15 +0900

    feat: DateInput / Enable time setting #402

--- client/src/components/Budget/Input/DateInput.tsx
+++ client/src/components/Budget/Input/DateInput.tsx
@@ -1,29 +1,35 @@
 import React, { useImperativeHandle, useRef } from 'react';
 
+interface DateInputProps {
+  className: string;
+  defaultValue?: Date | null;
+  required?: boolean;
+}
+
 const DateInput = React.forwardRef(
-    (
-        props: { className: string; defaultValue?: string; required?: boolean },
-        ref
-    ) => {
-        useImperativeHandle(ref, () => {
-            return {
-                value: () => dateRef.current!.value,
-            };
-        });
+  ({ className, defaultValue, required }: DateInputProps, ref) => {
+    useImperativeHandle(ref, () => {
+      return {
+        value: () => dateRef.current!.value,
+      };
+    });
 
-        const dateRef = useRef<HTMLInputElement>(null);
+    const dateRef = useRef<HTMLInputElement>(null);
 
-        return (
-            <input
-                className={props.className}
-                ref={dateRef}
-                type="date"
-                placeholder="날짜를 입력하세요"
-                defaultValue={props.defaultValue}
-                required={props.required}
-            />
-        );
-    }
+    return (
+      <input
+        className={className}
+        ref={dateRef}
+        type="datetime-local"
+        placeholder="날짜를 입력하세요"
+        defaultValue={
+          defaultValue?.toISOString().slice(0, 16) ||
+          new Date().toISOString().slice(0, 16)
+        }
+        required={required}
+      />
+    );
+  }
 );
 
 export default DateInput;
