a7aab8a92a9a015883b7c1990b807259fc154f2c
jessie129j@gmail.com <jessie129j@gmail.com>
2023-05-28 19:18:46 +0900

    feat: Setup jest #394

--- server/_mongoose/index.ts
+++ server/_mongoose/index.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 let isConnected = false;
 
+console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
 mongoose
   .connect(process.env.DB_URL)
   .then(() => {
--- server/setup.ts
+++ server/setup.ts
@@ -4,11 +4,11 @@ import { resolve } from "path";
 if (process.env.NODE_ENV?.trim() == "development") {
   dotenv.config({ path: __dirname + "/.env.development" });
 } else if (process.env.NODE_ENV?.trim() == "production") {
-  dotenv.config({ path: resolve(__dirname, "/.env.production") });
+  dotenv.config({ path: resolve(__dirname, "../.env.production") });
 } else if (process.env.NODE_ENV?.trim() == "local") {
-  dotenv.config({ path: resolve(__dirname, "/.env.local") });
+  dotenv.config({ path: resolve(__dirname + "/.env.local") });
 } else if (process.env.NODE_ENV?.trim() == "test") {
-  dotenv.config({ path: resolve(__dirname, "/.env.test") });
+  dotenv.config({ path: resolve(__dirname + "/.env.test") });
 } else {
   dotenv.config();
 }
