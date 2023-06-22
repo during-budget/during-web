2c50fbb786c98d3b08c485c2ced239183c69e250
jessie129j@gmail.com <jessie129j@gmail.com>
2023-06-22 21:44:13 +0900

    fix: fix typo #511

--- server/controllers/challenges.ts
+++ server/controllers/challenges.ts
@@ -7,7 +7,7 @@ import { IPaymentMethod, IUser, User } from "@models/User";
 import { logger } from "@logger";
 import { FIELD_REQUIRED, NOT_FOUND, NOT_PERMITTED } from "../@message";
 import { FIELD_INVALID } from "../@message";
-import { Challenge, IChallenge } from "@models/Chanllenge";
+import { Challenge, IChallenge } from "@models/Challenge";
 import { Transaction } from "@models/Transaction";
 
 const findTransactions = async (opts: {
