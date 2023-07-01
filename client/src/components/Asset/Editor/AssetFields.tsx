e8656e01f174309b1206722809dd16ea3af39476
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-07-01 19:46:11 +0900

    feat: AssetFields / Add AmountInput in AssetFields #179

--- client/src/components/Asset/Editor/AssetFields.tsx
+++ client/src/components/Asset/Editor/AssetFields.tsx
@@ -1,3 +1,4 @@
+import AmountInput from '../../Budget/Input/AmountInput';
 import classes from './AssetFields.module.css';
 
 interface AssetFieldsProps {
@@ -5,30 +6,9 @@ interface AssetFieldsProps {
   setAmount: (value: number) => void;
 }
 
-// TODO: NumberInput 개발 후 원 표시라던지 그런 거 수정
 const AssetFields = ({ amount, setAmount }: AssetFieldsProps) => {
-  const amountHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAmount(+event.target.value);
-  };
-
-  const focusHandler = (event: React.FocusEvent<HTMLInputElement>) => {
-    if (event.target.value === '0') {
-      event.target.value = '';
-    }
-  };
-
-  const blurHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (!event.target.value) {
-      setAmount(0);
-    }
-  };
-
-  const keyHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Backspace') {
-      if (event.currentTarget.value === '0') {
-        event.currentTarget.value = '';
-      }
-    }
+  const confirmHandler = (value: string) => {
+    setAmount(+value);
   };
 
   return (
@@ -36,15 +16,11 @@ const AssetFields = ({ amount, setAmount }: AssetFieldsProps) => {
       <label htmlFor="asset-field-amount" className={classes.label}>
         잔액
       </label>
-      <input
+      <AmountInput
         id="asset-field-amount"
         className={classes.amount}
-        type="number"
-        value={amount}
-        onChange={amountHandler}
-        onFocus={focusHandler}
-        onKeyUp={keyHandler}
-        onBlur={blurHandler}
+        defaultValue={amount.toString()}
+        onConfirm={confirmHandler}
       />
     </div>
   );
