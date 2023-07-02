04cc3907a80c55b0bce8cb011e01f36da3e7c3c1
jessie129j@gmail.com <jessie129j@gmail.com>
2023-07-02 22:28:15 +0900

    feat: implement theme setting validation #514

--- server/models/_basicSettings.ts
+++ server/models/_basicSettings.ts
@@ -2,3 +2,17 @@ export const basicChartSkin = "cat";
 export const chartSkins = ["basic", "cat", "bunny", "bear"];
 
 export const basicTimeZone = "Asia/Seoul";
+
+export const basicTheme = "basic";
+export const themes = [
+  "basic",
+  "mint",
+  "sky",
+  "blue",
+  "purple",
+  "pink",
+  "green",
+  "yellow",
+  "red",
+  "brown",
+];
