a31d7c6c7a4f394990bfb464aa30d85d1d35cb1d
jessie129j@gmail.com <jessie129j@gmail.com>
2023-07-05 23:46:49 +0900

    feat: create CCategory API #529

--- server/routes/categories.ts
+++ server/routes/categories.ts
@@ -12,6 +12,8 @@ router.get("/", isLoggedIn, categories.find);
 
 router.post("/", isLoggedIn, categories.create);
 
+router.get("/:_id", isLoggedIn, categories.findOne);
+
 router.put("/:_id", isLoggedIn, categories.update);
 
 router.delete("/:_id", isLoggedIn, categories.remove);
