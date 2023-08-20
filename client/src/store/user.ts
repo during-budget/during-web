f582cd47fa4e8e17d5dc019af2869349103eb6f3
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-08-20 11:43:29 +0900

    refactor: RequireAuth / Update login logic to set user data #592

--- client/src/store/user.ts
+++ client/src/store/user.ts
@@ -1,5 +1,6 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { AuthDataType, SnsIdType } from '../util/api/authAPI';
+import { AuthDataType } from '../util/api/authAPI';
+import { UserDataType } from '../util/api/userAPI';
 
 export interface UserInfoType {
   _id: string;
@@ -41,27 +42,38 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    login(state) {
+    login(state, action: PayloadAction<UserDataType>) {
       state.isAuth = true;
+
+      const {
+        _id,
+        userName,
+        email,
+        tel,
+        birthdate,
+        snsId,
+        isGuest,
+        isLocal,
+        basicBudgetId: defaultBudgetId,
+      } = action.payload;
+
+      state.info = {
+        _id,
+        defaultBudgetId,
+        userName,
+        email,
+        tel,
+        birthdate,
+      };
+      state.auth = {
+        email,
+        isLocal,
+        snsId,
+        isGuest,
+      };
     },
     logout(state) {
       state = initialState;
-    },
-    setUserInfo(state, action: PayloadAction<UserInfoType>) {
-      state.info = { ...action.payload };
-    },
-    updateUserInfo(state, action: PayloadAction<Partial<UserInfoType>>) {
-      state.info = { ...state.info, ...action.payload };
-    },
-    setAuthInfo(state, action: PayloadAction<AuthDataType>) {
-      const authInfo = action.payload;
-      state.auth = { ...action.payload };
-      if (!authInfo.snsId) {
-        state.auth = { ...state.auth, snsId: initialState.auth.snsId };
-      }
-    },
-    setSnsId(state, action: PayloadAction<SnsIdType>) {
-      state.auth.snsId = action.payload;
     },
   },
 });
