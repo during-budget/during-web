b4e465ce6ac71239896897fe0cdb5c7412843b5c
jessie129j@gmail.com <jessie129j@gmail.com>
2023-09-03 22:40:45 +0900

    feat: implement user setting services #597

--- server/src/api/routes/settings.ts
+++ server/src/api/routes/settings.ts
@@ -3,10 +3,11 @@ const router = express.Router();
 import { isLoggedIn } from "src/api/middleware/auth";
 
 import * as settings from "src/api/controllers/settings";
+import { wrapAsync } from "../middleware/error";
 
-router.get("/", isLoggedIn, settings.find);
-router.patch("/", isLoggedIn, settings.update);
+router.get("/", isLoggedIn, wrapAsync(settings.find));
+router.patch("/", isLoggedIn, wrapAsync(settings.update));
 
-router.get("/options", isLoggedIn, settings.options);
+router.get("/options", isLoggedIn, wrapAsync(settings.options));
 
 export default router;
