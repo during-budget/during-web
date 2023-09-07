ea1dec4269b298ab4382ea0ce24be24d061fcbd5
jessie129j@gmail.com <jessie129j@gmail.com>
2023-09-07 15:45:01 +0900

    feat: implement paymentmethod services #597

--- server/src/api/routes/paymentMethods.ts
+++ server/src/api/routes/paymentMethods.ts
@@ -3,8 +3,9 @@ const router = express.Router();
 import { isLoggedIn } from "src/api/middleware/auth";
 
 import * as paymentMethods from "src/api/controllers/paymentMethods";
+import { wrapAsync } from "../middleware/error";
 
-router.put("/", isLoggedIn, paymentMethods.update);
-router.get("/", isLoggedIn, paymentMethods.find);
+router.put("/", isLoggedIn, wrapAsync(paymentMethods.update));
+router.get("/", isLoggedIn, wrapAsync(paymentMethods.find));
 
 export default router;
