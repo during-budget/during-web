60ac1421156ecf49663af17b9780350e87515b5d
jessie129j@gmail.com <jessie129j@gmail.com>
2023-09-14 15:20:30 +0900

    feat: implement async handler #597

--- server/src/api/controllers/payments.ts
+++ server/src/api/controllers/payments.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import _ from "lodash";
 
-import { logger } from "src/api/middleware/loggers";
 import {
   FAKE_PAYMENT_ATTEMPT,
   FETCHING_ACCESSTOKEN_FAILED,
@@ -102,170 +101,145 @@ const getRawPayment = async (imp_uid: string) => {
 };
 
 export const prepare = async (req: Request, res: Response) => {
-  try {
-    /* validate */
-    if (!("itemTitle" in req.body)) {
-      return res.status(400).send({ message: FIELD_REQUIRED("itemTitle") });
-    }
-
-    const user = req.user!;
-
-    // if (user.isGuest) {
-    //   return res.status(403).send({ message: NOT_PERMITTED });
-    // }
-
-    if (
-      await Payment.findOne({
-        userId: user._id,
-        itemTitle: req.body.itemTitle,
-        status: "paid",
-      })
-    ) {
-      return res.status(409).send({ message: PAIED_ALREADY });
-    }
-
-    const item = await Item.findOne({ title: req.body.itemTitle });
-    if (!item) {
-      return res.status(404).send({ message: NOT_FOUND("item") });
-    }
-
-    /* create payment */
-    const payment = await Payment.create({
-      userId: user._id,
-      itemId: item._id,
-      itemType: item.type,
-      itemTitle: item.title,
-      status: "ready",
-      amount: item.price,
-    });
-
-    /* Pre-register payment information */
-    await preRegisterPaymentInfo(payment.merchant_uid, payment.amount);
-
-    return res.status(200).send({
-      payment,
-    });
-  } catch (err: any) {
-    logger.error(err.message);
-    return res.status(err.status ?? 500).send({ message: err.message });
+  /* validate */
+  if (!("itemTitle" in req.body)) {
+    return res.status(400).send({ message: FIELD_REQUIRED("itemTitle") });
   }
+
+  const user = req.user!;
+
+  // if (user.isGuest) {
+  //   return res.status(403).send({ message: NOT_PERMITTED });
+  // }
+
+  if (
+    await Payment.findOne({
+      userId: user._id,
+      itemTitle: req.body.itemTitle,
+      status: "paid",
+    })
+  ) {
+    return res.status(409).send({ message: PAIED_ALREADY });
+  }
+
+  const item = await Item.findOne({ title: req.body.itemTitle });
+  if (!item) {
+    return res.status(404).send({ message: NOT_FOUND("item") });
+  }
+
+  /* create payment */
+  const payment = await Payment.create({
+    userId: user._id,
+    itemId: item._id,
+    itemType: item.type,
+    itemTitle: item.title,
+    status: "ready",
+    amount: item.price,
+  });
+
+  /* Pre-register payment information */
+  await preRegisterPaymentInfo(payment.merchant_uid, payment.amount);
+
+  return res.status(200).send({
+    payment,
+  });
 };
 
 export const completeByUser = async (req: Request, res: Response) => {
-  try {
-    /* validate */
-    for (let field of ["imp_uid", "merchant_uid"]) {
-      if (!(field in req.body)) {
-        return res.status(400).send({ message: FIELD_REQUIRED(field) });
-      }
+  /* validate */
+  for (let field of ["imp_uid", "merchant_uid"]) {
+    if (!(field in req.body)) {
+      return res.status(400).send({ message: FIELD_REQUIRED(field) });
     }
-
-    const user = req.user!;
-
-    // 포트원 결제 정보 조회
-    const rawPaymentData = await getRawPayment(req.body.imp_uid);
-
-    // 포트원 결제 정보로 DB의 결제 정보 조회
-    const payment = await Payment.findById(rawPaymentData.merchant_uid);
-    if (!payment) {
-      return res.status(404).send({ message: NOT_FOUND("payment") });
-    }
-
-    if (!payment.userId.equals(user._id)) {
-      return res.status(403).send({ message: NOT_PERMITTED });
-    }
-
-    // 결제 정보 검증
-    if (rawPaymentData.amount === payment.amount) {
-      if (rawPaymentData.status === "paid") {
-        // 웹훅이 먼저 호출되지 않은 경우 DB에 결제 정보 저장
-        if (payment.status !== "paid") {
-          payment.status = "paid";
-          payment.rawPaymentData = rawPaymentData;
-          await payment.save();
-        }
-        return res.status(200).send({ payment });
-      }
-      return res.status(409).send({ message: PAYMENT_NOT_PAID });
-    }
-
-    return res.status(409).send({ message: FAKE_PAYMENT_ATTEMPT });
-  } catch (err: any) {
-    logger.error(err.message);
-    return res.status(err.status ?? 500).send({ message: err.message });
   }
-};
 
-export const completeByWebhook = async (req: Request, res: Response) => {
-  try {
-    /* validate */
-    for (let field of ["imp_uid", "merchant_uid", "status"]) {
-      if (!(field in req.body)) {
-        return res.status(400).send({ message: FIELD_REQUIRED(field) });
-      }
-    }
+  const user = req.user!;
 
-    // 포트원 결제 정보 조회
-    const rawPaymentData = await getRawPayment(req.body.imp_uid);
+  // 포트원 결제 정보 조회
+  const rawPaymentData = await getRawPayment(req.body.imp_uid);
 
-    // 포트원 결제 정보로 DB의 결제 정보 조회
-    const payment = await Payment.findById(rawPaymentData.merchant_uid);
-    if (!payment) {
-      return res.status(404).send({ message: NOT_FOUND("payment") });
-    }
+  // 포트원 결제 정보로 DB의 결제 정보 조회
+  const payment = await Payment.findById(rawPaymentData.merchant_uid);
+  if (!payment) {
+    return res.status(404).send({ message: NOT_FOUND("payment") });
+  }
 
-    // DB에 결제 정보 저장
-    if (rawPaymentData.amount === payment.amount) {
-      if (rawPaymentData.status === "paid") {
+  if (!payment.userId.equals(user._id)) {
+    return res.status(403).send({ message: NOT_PERMITTED });
+  }
+
+  // 결제 정보 검증
+  if (rawPaymentData.amount === payment.amount) {
+    if (rawPaymentData.status === "paid") {
+      // 웹훅이 먼저 호출되지 않은 경우 DB에 결제 정보 저장
+      if (payment.status !== "paid") {
         payment.status = "paid";
         payment.rawPaymentData = rawPaymentData;
         await payment.save();
-        return res.status(200).send();
       }
-      if (rawPaymentData.status === "cancelled") {
-        payment.status = "cancelled";
-        payment.rawPaymentData = rawPaymentData;
-        await payment.save();
-        return res.status(200).send();
-      }
-      return res.status(409).send({ message: PAYMENT_NOT_PAID });
+      return res.status(200).send({ payment });
     }
-    return res.status(409).send({ message: FAKE_PAYMENT_ATTEMPT });
-  } catch (err: any) {
-    logger.error(err.message);
-    return res.status(err.status ?? 500).send({ message: err.message });
+    return res.status(409).send({ message: PAYMENT_NOT_PAID });
   }
+
+  return res.status(409).send({ message: FAKE_PAYMENT_ATTEMPT });
+};
+
+export const completeByWebhook = async (req: Request, res: Response) => {
+  /* validate */
+  for (let field of ["imp_uid", "merchant_uid", "status"]) {
+    if (!(field in req.body)) {
+      return res.status(400).send({ message: FIELD_REQUIRED(field) });
+    }
+  }
+
+  // 포트원 결제 정보 조회
+  const rawPaymentData = await getRawPayment(req.body.imp_uid);
+
+  // 포트원 결제 정보로 DB의 결제 정보 조회
+  const payment = await Payment.findById(rawPaymentData.merchant_uid);
+  if (!payment) {
+    return res.status(404).send({ message: NOT_FOUND("payment") });
+  }
+
+  // DB에 결제 정보 저장
+  if (rawPaymentData.amount === payment.amount) {
+    if (rawPaymentData.status === "paid") {
+      payment.status = "paid";
+      payment.rawPaymentData = rawPaymentData;
+      await payment.save();
+      return res.status(200).send();
+    }
+    if (rawPaymentData.status === "cancelled") {
+      payment.status = "cancelled";
+      payment.rawPaymentData = rawPaymentData;
+      await payment.save();
+      return res.status(200).send();
+    }
+    return res.status(409).send({ message: PAYMENT_NOT_PAID });
+  }
+  return res.status(409).send({ message: FAKE_PAYMENT_ATTEMPT });
 };
 
 export const find = async (req: Request, res: Response) => {
-  try {
-    const user = req.user!;
+  const user = req.user!;
 
-    if ("userId" in req.query) {
-      if (user.auth !== "admin") {
-        return res.status(403).send({ message: NOT_PERMITTED });
-      }
-      const payments = await Payment.find({ userId: req.query.userId });
-      return res.status(200).send({ payments });
+  if ("userId" in req.query) {
+    if (user.auth !== "admin") {
+      return res.status(403).send({ message: NOT_PERMITTED });
     }
-    const payments = await Payment.find({ userId: user._id });
+    const payments = await Payment.find({ userId: req.query.userId });
     return res.status(200).send({ payments });
-  } catch (err: any) {
-    logger.error(err.message);
-    return res.status(500).send({ message: err.message });
   }
+  const payments = await Payment.find({ userId: user._id });
+  return res.status(200).send({ payments });
 };
 
 export const remove = async (req: Request, res: Response) => {
-  try {
-    const payment = await Payment.findByIdAndDelete(req.params._id);
+  const payment = await Payment.findByIdAndDelete(req.params._id);
 
-    if (!payment) {
-      return res.status(404).send({ message: NOT_FOUND("payment") });
-    }
-    return res.status(200).send({});
-  } catch (err: any) {
-    logger.error(err.message);
-    return res.status(500).send({ message: err.message });
+  if (!payment) {
+    return res.status(404).send({ message: NOT_FOUND("payment") });
   }
+  return res.status(200).send({});
 };
