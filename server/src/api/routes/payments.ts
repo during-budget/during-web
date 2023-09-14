4042a47fc9f96465856b79b6a14aa760b258f56b
jessie129j@gmail.com <jessie129j@gmail.com>
2023-09-14 15:20:30 +0900

    feat: implement async handler #597

--- server/src/api/routes/payments.ts
+++ server/src/api/routes/payments.ts
@@ -3,13 +3,18 @@ const router = express.Router();
 import { isLoggedIn, isPortOneWebHook, isAdmin } from "src/api/middleware/auth";
 
 import * as payments from "src/api/controllers/payments";
+import { wrapAsync } from "../middleware/error";
 
-router.post("/prepare", isLoggedIn, payments.prepare);
-router.post("/complete", isLoggedIn, payments.completeByUser);
-router.post("/complete/webhook", isPortOneWebHook, payments.completeByWebhook);
+router.post("/prepare", isLoggedIn, wrapAsync(payments.prepare));
+router.post("/complete", isLoggedIn, wrapAsync(payments.completeByUser));
+router.post(
+  "/complete/webhook",
+  isPortOneWebHook,
+  wrapAsync(payments.completeByWebhook)
+);
 
-router.get("/", isLoggedIn, payments.find);
+router.get("/", isLoggedIn, wrapAsync(payments.find));
 
-router.delete("/:_id", isAdmin, payments.remove);
+router.delete("/:_id", isAdmin, wrapAsync(payments.remove));
 
 export default router;
