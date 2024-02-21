cdf303da15d39854879f4bf7baefbbeb74eecef1
lynj <82319291+earlyoonj@users.noreply.github.com>
2024-02-22 07:51:24 +0900

    Feat: Add AssetCardListEditor to TransactionForm #623

--- client/src/components/Asset/Editor/AssetCardListEditor.tsx
+++ client/src/components/Asset/Editor/AssetCardListEditor.tsx
@@ -192,7 +192,7 @@ const AssetCardListEditor = ({
             ))}
         </DraggableList>
         <Button className={classes.add} styleClass="extra" onClick={openAddHandler}>
-          {isAsset ? '자산 ' : '결제수단 '} 추가하기
+          {isAsset ? '자산 ' : '카드 '} 추가하기
         </Button>
       </OverlayForm>
       <AssetCardItemEditor
