0bbb08ce827463be7e340e9412f7c87b5bed6e1a
lynj <82319291+earlyoonj@users.noreply.github.com>
2024-02-22 11:08:02 +0900

    Feat: Landing / Add CS button & email info #628

--- client/src/screens/Landing.tsx
+++ client/src/screens/Landing.tsx
@@ -8,6 +8,9 @@ import Terms from '../components/User/Info/Terms';
 import { useToggleOptions } from '../hooks/useToggle';
 import { loader as userLoader } from '../layout/Root';
 import { css } from '@emotion/react';
+import Channel from '../models/Channel';
+
+const { DURING_CHANNEL_KEY } = import.meta.env;
 
 const containerStyle = {
   maxWidth: 480,
@@ -21,11 +24,21 @@ const Landing = () => {
   const navigate = useNavigate();
   const userData = useLoaderData() as Awaited<ReturnType<typeof userLoader>>;
 
-  const [{ auth, terms, privacy }, openOverlay, closeOverlay] = useToggleOptions({
+  const [{ auth, terms, privacy }, open, close] = useToggleOptions({
     auth: false,
     terms: false,
     privacy: false,
   });
+
+  const openOverlay = (hash: string) => {
+    open(hash);
+    Channel.hideChannelButton();
+  };
+
+  const closeOverlay = (hash: string) => {
+    close(hash);
+    Channel.showChannelButton();
+  };
 
   useEffect(() => {
     if (!userData) {
@@ -35,12 +48,8 @@ const Landing = () => {
       // NOTE: 로그인된 사용자가 /landing으로 접근할 경우 서비스 화면으로 이동
       navigate('/budget', { replace: true });
     }
-  }, []);
 
-  const location = useLocation();
-  const hash = location.hash;
-
-  useEffect(() => {
+    // 약관 및 정책 오버레이 링크로 바로 이동 시 오버레이 뜨도록
     if (hash.includes('privacy-policy')) {
       openOverlay('privacy');
     }
@@ -48,7 +57,15 @@ const Landing = () => {
     if (hash.includes('terms')) {
       openOverlay('terms');
     }
+
+    // 채널톡
+    Channel.boot({
+      pluginKey: DURING_CHANNEL_KEY,
+    });
   }, []);
+
+  const location = useLocation();
+  const hash = location.hash;
 
   return (
     <div
@@ -58,25 +75,36 @@ const Landing = () => {
     >
       <img src="/images/logo.png" alt="듀링 가계부 로고" css={{ width: '2.5rem' }} />
       <LandingCarousel />
-      <Button onClick={openOverlay.bind(null, 'auth')} sizeClass="lg" css={css(buttonStyle)}>
+      <Button
+        onClick={openOverlay.bind(null, 'auth')}
+        sizeClass="lg"
+        css={css(buttonStyle)}
+      >
         듀링 가계부 시작하기
       </Button>
-      <div className="w-100 flex gap-md i-center">
+      <div className="w-100 flex gap-xs j-center i-center">
         <Button
+          className="text-md"
           onClick={openOverlay.bind(null, 'terms')}
           styleClass="extra"
-          css={css({ flexShrink: 1.3 })}
+          sizeClass='sm'
         >
           이용약관
         </Button>
         <span>{' | '}</span>
         <Button
-          className="mx-1.5"
+          className="mx-1.5 text-md"
           styleClass="extra"
+          sizeClass='sm'
           onClick={openOverlay.bind(null, 'privacy')}
         >
           개인정보처리방침
         </Button>
+      </div>
+      <div className="flex-column gap-sm">
+        <a href="mailto:dev.during@gmail.com" className="text-sm mt-0.3">
+          이메일 문의하기 - <u>dev.during@gmail.com</u>
+        </a>
       </div>
       <AuthOverlay isOpen={auth} onClose={closeOverlay.bind(null, 'auth')} />
       <Terms isOpen={terms} onClose={closeOverlay.bind(null, 'terms')} />
