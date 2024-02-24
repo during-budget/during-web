bf2afd6a3c3477133b91551537a414548d30f29e
jessie129j@gmail.com <jessie129j@gmail.com>
2024-02-24 15:39:37 +0900

    feat: implement agreement service

---
+++ server/src/api/services/agreement.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import { AgreementModel, AgreementEntity } from "@models/Agreement";
+import { Agreement, AgreementType } from "src/types/agreement";
+
+export const createAgreement = async (req: {
+  type: AgreementType;
+  version: string;
+}): Promise<AgreementEntity> => {
+  const { type, version } = req;
+
+  await AgreementModel.findOneAndUpdate(
+    {
+      type,
+      isDestroyed: false,
+    },
+    { isDestroyed: true }
+  );
+
+  const agreement = await AgreementModel.create({ type, version });
+
+  return agreement;
+};
+
+export const findAgreements = async (): Promise<{
+  termsOfUseAgreement: AgreementEntity;
+  privacyPolicyAgreement: AgreementEntity;
+}> => {
+  const [termsOfUseAgreement, privacyPolicyAgreement] = await Promise.all(
+    [AgreementType.TermsOfUse, AgreementType.PrivacyPolicy].map((type) =>
+      AgreementModel.findOne({
+        type,
+        isDestroyed: false,
+      })
+    )
+  );
+
+  if (!termsOfUseAgreement) {
+    throw new Error(
+      `Unexpectd Error; valid agreement ${AgreementType.TermsOfUse} not found`
+    );
+  }
+
+  if (!privacyPolicyAgreement) {
+    throw new Error(
+      `Unexpectd Error; valid agreement ${AgreementType.PrivacyPolicy} not found`
+    );
+  }
+
+  return { termsOfUseAgreement, privacyPolicyAgreement };
+};
