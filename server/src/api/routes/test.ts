0fd7e8464c9869aee3bf8a1ab91bf9aeec39c92b
JinseoJung <jessie129j@gmail.com>
2024-06-03 20:42:15 +0900

    [SERVER] Implement Logging (TEMP) (#668)
    * feat: enable logging
    
    * fix: add comments

--- server/src/api/routes/test.ts
+++ server/src/api/routes/test.ts
@@ -1,3 +1,4 @@
+import { hello } from "@controllers/test";
 import express, { Request, Response } from "express";
 // import * as TestController from "../controllers/test";
 // import { wrapAsync } from "../middleware/error";
@@ -5,6 +6,8 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/echo", async (req: Request, res: Response) => {
+  console.log("[TEST] echo!", { message: req.query.message });
+
   return res.status(200).send({ message: req.query.message });
 });
 
--- server/src/loaders/index.ts
+++ server/src/loaders/index.ts
@@ -5,12 +5,28 @@ import mongooseLoader from "./mongoose";
 import { loader as redisLoader } from "./redis";
 import loggerLoader from "./logger";
 import { configType } from "src/config/type";
+import morgan from "morgan";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export default async (app: Express, config: configType) => {
   passportLoader(config);
   await mongooseLoader(config);
   await redisLoader(config);
   // loggerLoader(config);
+
+  // NOTE: TEMP LOGGER
+  morgan.token("body", (req) => JSON.stringify((req as any).body));
+  morgan.token("time", () =>
+    dayjs().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss")
+  );
+  const customFormat =
+    ":time :method :url :status :res[content-length] - :response-time ms :body";
+  app.use(morgan(customFormat));
 
   await expressLoader(app, config);
 };
