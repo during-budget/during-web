a390ab9e7a11b80b6eed8ef6bc3f65fa389571d3
JinseoJung <jessie129j@gmail.com>
2024-07-10 12:22:08 +0900

    InApp / 인앱결제 모듈 구조 확장 (#675)
    * refactor: change InAppProductService to GoogleInAppHelper
    
    * fix: handle CompleteByUser request by template param
    
    * refactor: define payment types
    
    * refactor: handle request in router

---
+++ server/src/types/payment.ts
@@ -0,0 +1,40 @@
+export enum InAppPlatform {
+  Android = "android",
+  IOS = "ios",
+}
+
+export type CompletePaymentByAndroidReq = {
+  platform: InAppPlatform.Android;
+  title: string;
+  token: string;
+};
+
+export type IOSPayload = {
+  // transactionDate: Date;
+  // transactionId: string;
+  productId: string;
+  transactionReceipt: string;
+};
+
+export function validateIOSPayload(
+  payload: Record<string, any>
+): payload is IOSPayload {
+  if (!("productId" in payload) || typeof payload.productId !== "string")
+    return false;
+  if (
+    !("transactionReceipt" in payload) ||
+    typeof payload.transactionReceipt !== "string"
+  )
+    return false;
+
+  return true;
+}
+
+export type CompletePaymentByIOSReq = {
+  platform: InAppPlatform.IOS;
+  payload: IOSPayload;
+};
+
+export type CompletePaymentByMobileUserReq =
+  | CompletePaymentByAndroidReq
+  | CompletePaymentByIOSReq;
