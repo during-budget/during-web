a9e6832be093828522d466ee871d2a83c0d01612
jessie129j <jessie129j@gmail.com>
2024-07-10 12:14:11 +0900

    refactor: handle request in router

--- server/src/api/controllers/payments.ts
+++ server/src/api/controllers/payments.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import _ from "lodash";
 
-import { FieldInvalidError, FieldRequiredError } from "src/errors/InvalidError";
+import { FieldRequiredError } from "src/errors/InvalidError";
 
 import * as PaymentService from "src/services/payments";
 import * as ItemService from "src/services/items";
@@ -12,12 +12,6 @@ import {
 } from "src/errors/NotFoundError";
 import { AuthService } from "src/services/users";
 import { NotPermittedError } from "src/errors/ForbiddenError";
-import { isEnumValue } from "src/lib/const.lib";
-import {
-  InAppPlatform,
-  CompletePaymentByMobileUserReq,
-  validateIOSPayload,
-} from "src/types/payment";
 
 export const prepare = async (req: Request, res: Response) => {
   const user = req.user!;
@@ -70,60 +64,6 @@ export const completeByWebhook = async (req: Request, res: Response) => {
 
   await PaymentService.completePaymentByWebhook(imp_uid);
   return res.status(200).send();
-};
-
-export const completePaymentByMobileUser = async (
-  { user, body }: Request,
-  res: Response
-) => {
-  /** validateReq */
-  const validateReq = (body: Record<string, any>): void => {
-    /** validate platform */
-
-    if (!("platform" in body)) throw new FieldRequiredError("flatform");
-
-    const platform = body.platform;
-
-    if (typeof platform !== "string" || !isEnumValue(InAppPlatform, platform))
-      throw new FieldInvalidError("flatform");
-
-    switch (platform) {
-      case InAppPlatform.Android: {
-        for (let field of ["title", "token"]) {
-          if (!(field in body)) throw new FieldRequiredError(field);
-        }
-
-        return;
-      }
-
-      case InAppPlatform.IOS: {
-        for (let field of ["payload"]) {
-          if (!(field in body)) throw new FieldRequiredError(field);
-        }
-
-        if (!validateIOSPayload(body.payload)) {
-          throw new FieldInvalidError("payload");
-        }
-
-        return;
-      }
-
-      default: {
-        throw new Error(
-          `Unexpected Error; Not supported platform (${platform})`
-        );
-      }
-    }
-  };
-
-  validateReq(body);
-
-  const payment = await PaymentService.completePaymentByMobileUser(
-    body as CompletePaymentByMobileUserReq,
-    user!
-  );
-
-  return res.status(200).send({ payment });
 };
 
 export const find = async (req: Request, res: Response) => {
