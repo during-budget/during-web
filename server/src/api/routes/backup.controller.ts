d28f21f73d941d3e75ebe9a49ffbc8b3058f2b50
jessie129j <jessie129j@gmail.com>
2024-07-13 21:53:01 +0900

    fix: deactivate backup APIs

--- server/src/api/routes/backup.controller.ts
+++ server/src/api/routes/backup.controller.ts
@@ -5,27 +5,28 @@ import { BackupService } from "../services/backup.service";
 
 const router = express.Router();
 
-router.post(
-  "/backup",
-  isAdmin,
-  wrapAsync(async (req: Request, res: Response) => {
-    const { uuid, created, expired } = await BackupService.backup();
+// router.post(
+//   "/backup",
+//   isAdmin,
+//   wrapAsync(async (req: Request, res: Response) => {
+//     const { uuid, created, expired } = await BackupService.backup();
 
-    return res.status(200).send({
-      result: `${created} backupEntities with uuid ${uuid} are stored. ${expired} backupEntities are expired.`,
-    });
-  })
-);
+//     return res.status(200).send({
+//       result: `${created} backupEntities with uuid ${uuid} are stored. ${expired} backupEntities are expired.`,
+//     });
+//   })
+// );
 
-router.post(
-  "/restore",
-  isAdmin,
-  wrapAsync(async (req: Request, res: Response) => {
-    await BackupService.restore();
+// router.post(
+//   "/restore",
+//   isAdmin,
+//   wrapAsync(async (req: Request, res: Response) => {
+//     await BackupService.restore();
 
-    return res.status(200).send({
-      result: "Restored",
-    });
-  })
-);
+//     return res.status(200).send({
+//       result: "Restored",
+//     });
+//   })
+// );
+
 export default router;
