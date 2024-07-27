a16f49ffcc44bef905c35442cffe0b58c540adcb
JinseoJung <jessie129j@gmail.com>
2024-07-27 17:03:49 +0900

    Payment / CompleteByMobileUser API 확장 - IOS 지원 (#680)
    * feat: implement AppleReceiptValidator
    
    * feat: implement Apple Shared Secret (env)
    
    * fix: add isSendbox bodyParam to CompleteByMobileUser (ios)
    
    * feat: add iap errors
    
    * feat: implement IOS payment validation

--- server/src/api/routes/payments.ts
+++ server/src/api/routes/payments.ts
@@ -56,7 +56,7 @@ router.post(
           }
 
           case InAppPlatform.IOS: {
-            for (let field of ["payload"]) {
+            for (let field of ["payload", "isSendbox"]) {
               if (!(field in body)) throw new FieldRequiredError(field);
             }
 
